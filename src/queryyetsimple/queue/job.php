a495cfde65dad96b266fa7f259a01e4149d879d2
Xiangmin Liu <l>
2017-06-07 08:50:06 +0800

    删除多余的代码

--- src/queryyetsimple/queue/job.php
+++ src/queryyetsimple/queue/job.php
@@ -157,27 +157,4 @@ abstract class job extends PHPQueueJob implements interfaces_job {
     protected function getJob($strJob) {
         return project ()->make ( $strJob );
     }
-    
-    /**
-     * Calculate the number of seconds with the given delay.
-     *
-     * @param \DateTime|int $delay            
-     * @return int
-     */
-    protected function getSeconds($delay) {
-        if ($delay instanceof DateTime) {
-            return max ( 0, $delay->getTimestamp () - $this->getTime () );
-        }
-        
-        return ( int ) $delay;
-    }
-    
-    /**
-     * Get the current system time.
-     *
-     * @return int
-     */
-    protected function getTime() {
-        return time ();
-    }
 }
