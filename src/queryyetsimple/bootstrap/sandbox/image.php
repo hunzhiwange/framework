21f4cf63363ef46b33a2956e5d4308d232e154fa
Xiangmin Liu <l>
2017-06-21 12:13:24 +0800

    complete the image component

---
+++ src/queryyetsimple/bootstrap/sandbox/image.php
@@ -0,0 +1,42 @@
+<?php
+// [$QueryPHP] A PHP Framework Since 2010.10.03. <Query Yet Simple>
+// ©2010-2017 http://queryphp.com All rights reserved.
+namespace queryyetsimple;
+
+<<<queryphp
+##########################################################
+#   ____                          ______  _   _ ______   #
+#  /     \       ___  _ __  _   _ | ___ \| | | || ___ \  #
+# |   (  ||(_)| / _ \| '__|| | | || |_/ /| |_| || |_/ /  #
+#  \____/ |___||  __/| |   | |_| ||  __/ |  _  ||  __/   #
+#       \__   | \___ |_|    \__  || |    | | | || |      #
+#     Query Yet Simple      __/  |\_|    |_| |_|\_|      #
+#                          |___ /  Since 2010.10.03      #
+##########################################################
+queryphp;
+
+use queryyetsimple\image\image as image_image;
+
+/**
+ * 沙盒 image
+ *
+ * @author Xiangmin Liu<635750556@qq.com>
+ * @package $$
+ * @since 2017.06.10
+ * @version 1.0
+ */
+class image extends image_image {
+}
+
+namespace qys;
+
+/**
+ * 沙盒 image
+ *
+ * @author Xiangmin Liu<635750556@qq.com>
+ * @package $$
+ * @since 2017.06.10
+ * @version 1.0
+ */
+class image extends \queryyetsimple\image {
+}
