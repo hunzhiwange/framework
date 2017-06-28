9c72a4e26e215f12c11d706cc412f17d62ed3d62
Xiangmin Liu <l>
2017-06-28 18:53:39 +0800

    add arrayable interfaces

---
+++ src/queryyetsimple/support/interfaces/arrayable.php
@@ -0,0 +1,34 @@
+<?php
+// [$QueryPHP] The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+// ©2010-2017 http://queryphp.com All rights reserved.
+namespace queryyetsimple\support\interfaces;
+
+<<<queryphp
+##########################################################
+#   ____                          ______  _   _ ______   #
+#  /     \       ___  _ __  _   _ | ___ \| | | || ___ \  #
+# |   (  ||(_)| / _ \| '__|| | | || |_/ /| |_| || |_/ /  #
+#  \____/ |___||  __/| |   | |_| ||  __/ |  _  ||  __/   #
+#       \__   | \___ |_|    \__  || |    | | | || |      #
+#     Query Yet Simple      __/  |\_|    |_| |_|\_|      #
+#                          |___ /  Since 2010.10.03      #
+##########################################################
+queryphp;
+
+/**
+ * arrayable 接口
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ * @package $$
+ * @since 2017.04.13
+ * @version 1.0
+ */
+interface arrayable {
+    
+    /**
+     * 对象转数组
+     *
+     * @return array
+     */
+    public function toArray();
+}
