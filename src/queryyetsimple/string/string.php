a395b52eeca9ce29cd8e213cbb99f7b93fab9bec
Xiangmin Liu <l>
2017-08-07 09:01:10 +0800

    string add some rand method

--- src/queryyetsimple/string/string.php
+++ src/queryyetsimple/string/string.php
@@ -30,33 +30,155 @@ class string {
     use infinity;
     
     /**
+     * 随机字母数字
+     *
+     * @param int $nLength            
+     * @param string $sCharBox            
+     * @return string
+     */
+    public static function randAlphaNum($nLength, $sCharBox = null) {
+        if (! $nLength)
+            return false;
+        if (is_null ( $sCharBox )) {
+            $sCharBox = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        }
+        return static::randSting ( $nLength, $sCharBox );
+    }
+    
+    /**
+     * 随机小写字母数字
+     *
+     * @param int $nLength            
+     * @param string $sCharBox            
+     * @return string
+     */
+    public static function randAlphaNumLowercase($nLength, $sCharBox = null) {
+        if (! $nLength)
+            return false;
+        if (is_null ( $sCharBox )) {
+            $sCharBox = 'abcdefghijklmnopqrstuvwxyz1234567890';
+        }
+        return static::randSting ( $nLength, $sCharBox );
+    }
+    
+    /**
+     * 随机大写字母数字
+     *
+     * @param int $nLength            
+     * @param string $sCharBox            
+     * @return string
+     */
+    public static function randAlphaNumUppercase($nLength, $sCharBox = null) {
+        if (! $nLength)
+            return false;
+        if (is_null ( $sCharBox )) {
+            $sCharBox = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        }
+        return static::randSting ( $nLength, $sCharBox );
+    }
+    
+    /**
+     * 随机字母
+     *
+     * @param int $nLength            
+     * @param string $sCharBox            
+     * @return string
+     */
+    public static function randAlpha($nLength, $sCharBox = null) {
+        if (! $nLength)
+            return false;
+        if (is_null ( $sCharBox )) {
+            $sCharBox = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        }
+        return static::randSting ( $nLength, $sCharBox );
+    }
+    
+    /**
+     * 随机小写字母
+     *
+     * @param int $nLength            
+     * @param string $sCharBox            
+     * @return string
+     */
+    public static function randAlphaLowercase($nLength, $sCharBox = null) {
+        if (! $nLength)
+            return false;
+        if (is_null ( $sCharBox )) {
+            $sCharBox = 'abcdefghijklmnopqrstuvwxyz';
+        } else {
+            $sCharBox = strtolower ( $sCharBox );
+        }
+        return static::randSting ( $nLength, $sCharBox );
+    }
+    
+    /**
+     * 随机大写字母
+     *
+     * @param int $nLength            
+     * @param string $sCharBox            
+     * @return string
+     */
+    public static function randAlphaUppercase($nLength, $sCharBox = null) {
+        if (! $nLength)
+            return false;
+        if (is_null ( $sCharBox )) {
+            $sCharBox = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        } else {
+            $sCharBox = strtoupper ( $sCharBox );
+        }
+        return static::randSting ( $nLength, $sCharBox );
+    }
+    
+    /**
+     * 随机数字
+     *
+     * @param int $nLength            
+     * @param string $sCharBox            
+     * @return string
+     */
+    public static function randNum($nLength, $sCharBox = null) {
+        if (! $nLength)
+            return false;
+        if (is_null ( $sCharBox )) {
+            $sCharBox = '0123456789';
+        }
+        return static::randSting ( $nLength, $sCharBox );
+    }
+    
+    /**
+     * 随机字中文
+     *
+     * @param int $nLength            
+     * @param string $sCharBox            
+     * @return string
+     */
+    public static function randChinese($nLength, $sCharBox = null) {
+        if (! $nLength)
+            return false;
+        
+        if (is_null ( $sCharBox )) {
+            $sCharBox = '在了不和有大这主中人上为们地个用工时要动国产以我到他会作来分生对于学下级就年阶义发成部民可出能方进同行面说种过命度革而多子后自社加小机也经力线本电高量长党得实家定深法表着水理化争现所二起政三好十战无农使性前等反体合斗路图把结第里正新开论之物从当两些还天资事队批如应形想制心样干都向变关点育重其思与间内去因件日利相由压员气业代全组数果期导平各基或月毛然问比展那它最及外没看治提五解系林者米群头意只明四道马认次文通但条较克又公孔领军流入接席位情运器并飞原油放立题质指建区验活众很教决特此常石强极土少已根共直团统式转别造切九您取西持总料连任志观调七么山程百报更见必真保热委手改管处己将修支识病象几先老光专什六型具示复安带每东增则完风回南广劳轮科北打积车计给节做务被整联步类集号列温装即毫知轴研单色坚据速防史拉世设达尔场织历花受求传口断况采精金界品判参层止边清至万确究书术状厂须离再目海交权且儿青才证低越际八试规斯近注办布门铁需走议县兵固除般引齿千胜细影济白格效置推空配刀叶率述今选养德话查差半敌始片施响收华觉备名红续均药标记难存测士身紧液派准斤角降维板许破述技消底床田势端感往神便贺村构照容非搞亚磨族火段算适讲按值美态黄易彪服早班麦削信排台声该击素张密害侯草何树肥继右属市严径螺检左页抗苏显苦英快称坏移约巴材省黑武培着河帝仅针怎植京助升王眼她抓含苗副杂普谈围食射源例致酸旧却充足短划剂宣环落首尺波承粉践府鱼随考刻靠够满夫失包住促枝局菌杆周护岩师举曲春元超负砂封换太模贫减阳扬江析亩木言球朝医校古呢稻宋听唯输滑站另卫字鼓刚写刘微略范供阿块某功套友限项余倒卷创律雨让骨远帮初皮播优占死毒圈伟季训控激找叫云互跟裂粮粒母练塞钢顶策双留误础吸阻故寸盾晚丝女散焊功株亲院冷彻弹错散商视艺灭版烈零室轻血倍缺厘泵察绝富城冲喷壤简否柱李望盘磁雄似困巩益洲脱投送奴侧润盖挥距触星松送获兴独官混纪依未突架宽冬章湿偏纹吃执阀矿寨责熟稳夺硬价努翻奇甲预职评读背协损棉侵灰虽矛厚罗泥辟告卵箱掌氧恩爱停曾溶营终纲孟钱待尽俄缩沙退陈讨奋械载胞幼哪剥迫旋征槽倒握担仍呀鲜吧卡粗介钻逐弱脚怕盐末阴丰编印蜂急拿扩伤飞露核缘游振操央伍域甚迅辉异序免纸夜乡久隶缸夹念兰映沟乙吗儒杀汽磷艰晶插埃燃欢铁补咱芽永瓦倾阵碳演威附牙芽永瓦斜灌欧献顺猪洋腐请透司危括脉宜笑若尾束壮暴企菜穗楚汉愈绿拖牛份染既秋遍锻玉夏疗尖殖井费州访吹荣铜沿替滚客召旱悟刺脑的一是';
+        }
+        
+        $sRet = '';
+        for($int = 0; $int < $nLength; $int ++) {
+            $sRet .= static::subString ( $sCharBox, floor ( mt_rand ( 0, mb_strlen ( $sCharBox, 'utf-8' ) - 1 ) ), 1 );
+        }
+        unset ( $sCharBox );
+        return $sRet;
+    }
+    
+    /**
      * 随机字符串
      *
      * @param int $nLength            
      * @param string $sCharBox            
-     * @param boolean $bNumeric            
      * @return string
      */
-    public static function randString($nLength, $sCharBox = null, $bNumeric = false) {
-        if ($bNumeric === true) {
-            return sprintf ( '%0' . $nLength . 'd', mt_rand ( 1, pow ( 10, $nLength ) - 1 ) );
-        }
-        
-        if ($sCharBox === null) {
-            list ( $nMS, $nS ) = explode ( ' ', microtime () );
-            $nCurTime = $nS + $nMS;
-            
-            $sCharBox = strtoupper ( md5 ( $nCurTime . rand ( 1000000000, 9999999999 ) ) );
-            $sCharBox .= md5 ( $nCurTime . rand ( 1000000000, 9999999999 ) );
-        }
-        
-        $nBoxEnd = strlen ( $sCharBox ) - 1;
-        $sRet = '';
-        while ( $nLength -- ) {
-            $sRet .= substr ( $sCharBox, rand ( 0, $nBoxEnd ), 1 );
-        }
-        
-        return $sRet;
+    public static function randSting($nLength, $sCharBox) {
+        if (! $nLength || ! $sCharBox)
+            return false;
+        return substr ( str_shuffle ( str_repeat ( $sCharBox, round ( $nLength / count ( $sCharBox ) ) ) ), 0, $nLength );
     }
     
     /**
@@ -166,6 +288,7 @@ class string {
         
         return $sSlice;
     }
+    
     /**
      * 日期格式化
      *
@@ -216,20 +339,6 @@ class string {
     }
     
     /**
-     * 判断字符串中是否包含给定的字符开始
-     *
-     * @param string $strToSearched            
-     * @param string $strSearch            
-     * @return bool
-     */
-    public static function startsWith($strToSearched, $strSearch) {
-        if ($strSearch != '' && strpos ( $strToSearched, $strSearch ) === 0) {
-            return true;
-        }
-        return false;
-    }
-    
-    /**
      * 下划线转驼峰
      *
      * @param string $strValue            
@@ -250,6 +359,20 @@ class string {
      */
     public static function unCamelize($strValue, $strSeparator = '_') {
         return strtolower ( preg_replace ( '/([a-z])([A-Z])/', "$1" . $strSeparator . "$2", $strValue ) );
+    }
+    
+    /**
+     * 判断字符串中是否包含给定的字符开始
+     *
+     * @param string $strToSearched            
+     * @param string $strSearch            
+     * @return bool
+     */
+    public static function startsWith($strToSearched, $strSearch) {
+        if ($strSearch != '' && strpos ( $strToSearched, $strSearch ) === 0) {
+            return true;
+        }
+        return false;
     }
     
     /**
