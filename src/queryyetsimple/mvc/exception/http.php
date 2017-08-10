4f6ab72bebc3ca778b8227b074dbc8e2987a09e7
Xiangmin Liu <l>
2017-08-10 22:26:49 +0800

    add exception http and too_many_requests_http

---
+++ src/queryyetsimple/mvc/exception/http.php
@@ -0,0 +1,60 @@
+<?php
+// [$QueryPHP] The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+// ©2010-2017 http://queryphp.com All rights reserved.
+namespace queryyetsimple\mvc\exception;
+
+<<<queryphp
+##########################################################
+#   ____                          ______  _   _ ______   #
+#  /     \       ___  _ __  _   _ | ___ \| | | || ___ \  #
+# |   (  ||(_)| / _ \| '__|| | | || |_/ /| |_| || |_/ /  #
+#  \____/ |___||  __/| |   | |_| ||  __/ |  _  ||  __/   #
+#       \__   | \___ |_|    \__  || |    | | | || |      #
+#     Query Yet Simple      __/  |\_|    |_| |_|\_|      #
+#                          |___ /  Since 2010.10.03      #
+##########################################################
+queryphp;
+
+use Exception;
+use RuntimeException;
+
+/**
+ * HTTP 异常
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ * @package $$
+ * @since 2017.08.10
+ * @version 1.0
+ */
+class http extends RuntimeException {
+    
+    /**
+     * HTTP 状态
+     *
+     * @var int
+     */
+    protected $intStatusCode;
+    
+    /**
+     * 构造函数
+     *
+     * @param int $intStatusCode            
+     * @param string|null $strMessage            
+     * @param integer $intCode            
+     * @param \Exception $objPrevious            
+     * @return void
+     */
+    public function __construct($intStatusCode, $strMessage = null, $intCode = 0, Exception $objPrevious = null) {
+        $this->intStatusCode = $intStatusCode;
+        parent::__construct ( $strMessage, $intCode, $objPrevious );
+    }
+    
+    /**
+     * HTTP 状态
+     *
+     * @return void
+     */
+    public function statusCode() {
+        return $this->intStatusCode;
+    }
+}
---
+++ src/queryyetsimple/mvc/exception/too_many_requests_http.php
@@ -0,0 +1,43 @@
+<?php
+// [$QueryPHP] The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+// ©2010-2017 http://queryphp.com All rights reserved.
+namespace queryyetsimple\mvc\exception;
+
+<<<queryphp
+##########################################################
+#   ____                          ______  _   _ ______   #
+#  /     \       ___  _ __  _   _ | ___ \| | | || ___ \  #
+# |   (  ||(_)| / _ \| '__|| | | || |_/ /| |_| || |_/ /  #
+#  \____/ |___||  __/| |   | |_| ||  __/ |  _  ||  __/   #
+#       \__   | \___ |_|    \__  || |    | | | || |      #
+#     Query Yet Simple      __/  |\_|    |_| |_|\_|      #
+#                          |___ /  Since 2010.10.03      #
+##########################################################
+queryphp;
+
+use Exception;
+
+/**
+ * 请求过于频繁异常
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ * @package $$
+ * @since 2017.08.10
+ * @version 1.0
+ */
+class too_many_requests_http extends http {
+    
+    /**
+     * 构造函数
+     *
+     * @param int $intStatusCode            
+     * @param string|null $strMessage            
+     * @param \Exception $objPrevious            
+     * @param array $arrHeader            
+     * @param integer $intCode            
+     * @return void
+     */
+    public function __construct($strMessage = null, $intCode = 0, Exception $objPrevious = null) {
+        parent::__construct ( 429, $strMessage, $intCode, $objPrevious );
+    }
+}
