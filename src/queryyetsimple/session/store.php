72ab6f746c68c889979dede1c54037a8bb72a845
Xiangmin Liu <l>
2017-08-19 14:32:00 +0800

    ditto

--- src/queryyetsimple/session/store.php
+++ src/queryyetsimple/session/store.php
@@ -75,11 +75,11 @@ class store implements interfaces_store {
     /**
      * 启动 session
      *
-     * @return void
+     * @return $this
      */
     public function start() {
         if ($this->isStart ()) {
-            return;
+            return $this;
         }
         
         // 设置 session 不自动启动
@@ -146,8 +146,10 @@ class store implements interfaces_store {
         
         // 启动 session
         if (! session_start ()) {
-            return null;
+            throw new RuntimeException ( 'Session start failed' );
         }
+        
+        return $this;
     }
     
     /**
@@ -187,6 +189,80 @@ class store implements interfaces_store {
     }
     
     /**
+     * 数组插入数据
+     *
+     * @param string $strKey            
+     * @param mixed $mixValue            
+     * @return void
+     */
+    public function push($strKey, $mixValue) {
+        $arr = $this->get ( $strKey, [ ] );
+        $arr [] = $mixValue;
+        $this->set ( $strKey, $arr );
+    }
+    
+    /**
+     * 合并元素
+     *
+     * @param string $strKey            
+     * @param array $arrValue            
+     * @return void
+     */
+    public function merge($strKey, array $arrValue) {
+        $this->set ( $strKey, array_unique ( array_merge ( $this->get ( $strKey, [ ] ), $arrValue ) ) );
+    }
+    
+    /**
+     * 弹出元素
+     *
+     * @param string $strKey            
+     * @param mixed $mixValue            
+     * @return void
+     */
+    public function pop($strKey, array $arrValue) {
+        $this->set ( $strKey, array_diff ( $this->get ( $strKey, [ ] ), $arrValue ) );
+    }
+    
+    /**
+     * 数组插入键值对数据
+     *
+     * @param string $strKey            
+     * @param mixed $mixKey            
+     * @param mixed $mixValue            
+     * @return void
+     */
+    public function arrays($strKey, $mixKey, $mixValue = null) {
+        $arr = $this->get ( $strKey, [ ] );
+        if (is_string ( $mixKey )) {
+            $arr [$mixKey] = $mixValue;
+        } elseif (is_array ( $mixKey )) {
+            $arr = array_merge ( $arr, $mixKey );
+        }
+        $this->set ( $strKey, $arr );
+    }
+    
+    /**
+     * 数组键值删除数据
+     *
+     * @param string $strKey            
+     * @param mixed $mixKey            
+     * @return void
+     */
+    public function arraysDelete($strKey, $mixKey) {
+        $arr = $this->get ( $strKey, [ ] );
+        if (! is_array ( $mixKey )) {
+            $mixKey = [ 
+                    $mixKey 
+            ];
+        }
+        foreach ( $mixKey as $strFoo ) {
+            if (isset ( $arr [$strFoo] ))
+                unset ( $arr [$strFoo] );
+        }
+        $this->set ( $strKey, $arr );
+    }
+    
+    /**
      * 取回 session
      *
      * @param string $sName            
@@ -205,14 +281,21 @@ class store implements interfaces_store {
      * 删除 session
      *
      * @param string $sName            
+     * @param boolean $bPrefix            
      * @return bool
      */
-    public function delete($sName) {
+    public function delete($sName, $bPrefix = true) {
         $this->checkStart ();
         
         assert::string ( $sName );
-        $sName = $this->getName ( $sName );
-        return session_unregister ( $sName );
+        if ($bPrefix) {
+            $sName = $this->getName ( $sName );
+        }
+        
+        if (isset ( $_SESSION [$sName] ))
+            unset ( $_SESSION [$sName] );
+        
+        return true;
     }
     
     /**
@@ -244,6 +327,135 @@ class store implements interfaces_store {
                 $this->delete ( $sKey, false );
             else
                 $this->delete ( $sKey, false );
+        }
+    }
+    
+    /**
+     * 闪存一个数据，当前请求和下一个请求可用
+     *
+     * @param string $strKey            
+     * @param mixed $mixValue            
+     * @return void
+     */
+    public function flash($strKey, $mixValue = null) {
+        if (is_null ( $mixValue )) {
+            return $this->getFlash ( $strKey );
+        } else {
+            $this->set ( $this->flashDataKey ( $strKey ), $mixValue );
+            $this->mergeNewFlash ( [ 
+                    $strKey 
+            ] );
+            $this->popOldFlash ( [ 
+                    $strKey 
+            ] );
+        }
+    }
+    
+    /**
+     * 批量闪存一个数据，当前请求和下一个请求可用
+     *
+     * @param array $arrFlash            
+     * @return void
+     */
+    public function flashs(array $arrFlash) {
+        foreach ( $arrFlash as $strKey => $mixValue ) {
+            $this->flash ( $strKey, $mixValue );
+        }
+    }
+    
+    /**
+     * 闪存一个 flash 用于当前请求使用，下一个请求将无法获取
+     *
+     * @param string $strKey            
+     * @param mixed $mixValue            
+     * @return void
+     */
+    public function nowFlash($strKey, $mixValue) {
+        $this->set ( $this->flashDataKey ( $strKey ), $mixValue );
+        $this->mergeOldFlash ( [ 
+                $strKey 
+        ] );
+    }
+    
+    /**
+     * 保持所有闪存数据
+     *
+     * @return void
+     */
+    public function rebuildFlash() {
+        $this->mergeNewFlash ( $this->get ( $this->flashOldKey (), [ ] ) );
+        $this->set ( $this->flashOldKey (), [ ] );
+    }
+    
+    /**
+     * 保持闪存数据
+     *
+     * @param mixed $mixKey            
+     * @return void
+     */
+    public function keepFlash($mixKey) {
+        $mixKey = is_array ( $mixKey ) ? $mixKey : func_get_args ();
+        $this->mergeNewFlash ( $mixKey );
+        $this->popOldFlash ( $mixKey );
+    }
+    
+    /**
+     * 返回闪存数据
+     *
+     * @param string $strKey            
+     * @param mixed $mixDefault            
+     * @return void
+     */
+    public function getFlash($strKey, $mixDefault = null) {
+        return $this->get ( $this->flashDataKey ( $strKey ), $mixDefault );
+    }
+    
+    /**
+     * 删除闪存数据
+     *
+     * @param mixed $mixKey            
+     * @return void
+     */
+    public function deleteFlash($mixKey) {
+        $mixKey = is_array ( $mixKey ) ? $mixKey : func_get_args ();
+        
+        foreach ( $mixKey as $strKey ) {
+            $this->delete ( $this->flashDataKey ( $strKey ) );
+        }
+        
+        $this->mergeOldFlash ( $mixKey );
+        $this->popNewFlash ( $mixKey );
+    }
+    
+    /**
+     * 清理所有闪存数据
+     *
+     * @return void
+     */
+    public function clearFlash() {
+        foreach ( $this->get ( $this->flashNewKey (), [ ] ) as $strNew ) {
+            $this->deleteFlash ( $strNew );
+        }
+    }
+    
+    /**
+     * 程序执行结束清理 flash
+     *
+     * @return void
+     */
+    public function unregisterFlash() {
+        if ($this->isStart ()) {
+            $arr = $this->get ( $this->flashNewKey (), [ ] );
+            $arrOld = $this->get ( $this->flashOldKey (), [ ] );
+            
+            foreach ( $arrOld as $strOld ) {
+                $this->delete ( $this->flashDataKey ( $strOld ) );
+            }
+            
+            $this->delete ( $this->flashNewKey () );
+            $this->set ( $this->flashOldKey (), $arr );
+            
+            unset ( $arr, $arrOld );
         }
     }
     
@@ -461,5 +673,73 @@ class store implements interfaces_store {
         if (! $this->isStart ()) {
             throw new RuntimeException ( 'Session is not start yet' );
         }
+    }
+    
+    /**
+     * 弹出旧闪存 KEY
+     *
+     * @param array $arrKey            
+     * @return void
+     */
+    protected function popOldFlash(array $arrKey) {
+        $this->pop ( $this->flashOldKey (), $arrKey );
+    }
+    
+    /**
+     * 合并旧闪存 KEY
+     *
+     * @param array $arrKey            
+     * @return void
+     */
+    protected function mergeOldFlash(array $arrKey) {
+        $this->merge ( $this->flashOldKey (), $arrKey );
+    }
+    
+    /**
+     * 弹出新闪存 KEY
+     *
+     * @param array $arrKey            
+     * @return void
+     */
+    protected function popNewFlash(array $arrKey) {
+        $this->pop ( $this->flashNewKey (), $arrKey );
+    }
+    
+    /**
+     * 合并新闪存 KEY
+     *
+     * @param array $arrKey            
+     * @return void
+     */
+    protected function mergeNewFlash(array $arrKey) {
+        $this->merge ( $this->flashNewKey (), $arrKey );
+    }
+    
+    /**
+     * 闪存值 KEY
+     *
+     * @param string $strKey            
+     * @return string
+     */
+    protected function flashDataKey($strKey) {
+        return 'flash.data.' . $strKey;
+    }
+    
+    /**
+     * 新值闪存 KEY
+     *
+     * @return string
+     */
+    protected function flashNewKey() {
+        return 'flash.new.key';
+    }
+    
+    /**
+     * 旧值闪存 KEY
+     *
+     * @return string
+     */
+    protected function flashOldKey() {
+        return 'flash.old.key';
     }
 }
