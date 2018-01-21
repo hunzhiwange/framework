0f201e5b17efc753fb03161cd4d50ac19d4ee4b8
Xiangmin Liu <l>
2018-01-21 12:38:26 +0800

    to psr4

--- Provider/Register.php
+++ Provider/Register.php
@@ -17,22 +17,58 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace tests;
+namespace Queryyetsimple\Validate\Provider;
 
-use queryyetsimple\{
-    psr4,
-    router
+use Queryyetsimple\{
+    Support\Provider,
+    Validate\Validate
 };
 
 /**
- * phpunit 内部启动文件
+ * validate 服务提供者
  *
  * @author Xiangmin Liu <635750556@qq.com>
  * @package $$
- * @since 2017.05.09
+ * @since 2017.07.26
  * @version 1.0
  */
-psr4::import('tests', dirname(env('app_bootstrap')));
-router::bind('phpunittests://bootstrap/index', function () {
-    return (new application())->run();
-});
+class Register extends Provider
+{
+
+    /**
+     * 注册服务
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->singleton('validate', function ($project) {
+            return (new Validate())->container($project);
+        });
+    }
+
+    /**
+     * 载入语言包
+     *
+     * @return void
+     */
+    public function bootstrap()
+    {
+        $this->loadI18nDir(__DIR__ . '/../i18n');
+    }
+
+    /**
+     * 可用服务提供者
+     *
+     * @return array
+     */
+    public static function providers()
+    {
+        return [
+            'validate' => [
+                'Queryyetsimple\Validate\Validate',
+                'Queryyetsimple\Validate\IValidate'
+            ]
+        ];
+    }
+}
--- ValidateFailed.php
+++ ValidateFailed.php
@@ -17,54 +17,67 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace tests;
+namespace Queryyetsimple\Validate;
 
-use PHPUnit_Framework_TestCase;
+use Exception;
 
 /**
- * phpunit 测试用例
+ * 验证异常
  *
  * @author Xiangmin Liu <635750556@qq.com>
  * @package $$
- * @since 2017.05.09
+ * @since 2017.08.25
  * @version 1.0
  */
-abstract class testcase extends PHPUnit_Framework_TestCase
+class ValidateFailed extends Exception
 {
 
     /**
-     * setUpBeforeClass
+     * 验证器
      *
+     * @var \Queryyetsimple\Validate\IValidate
+     */
+    public $objValidate;
+
+    /**
+     * 响应组件
+     *
+     * @var \Queryyetsimple\Http\Response|null
+     */
+    public $objResponse;
+
+    /**
+     * 构造函数
+     *
+     * @param \Queryyetsimple\Validate\IValidate $objValidate
+     * @param \Queryyetsimple\Http\Response $objResponse
      * @return void
      */
-    public static function setUpBeforeClass()
+    public function __construct($objValidate, $objResponse = null)
     {
+        parent::__construct('Validate failed');
+
+        $this->objResponse = $objResponse;
+        $this->objValidate = $objValidate;
     }
 
     /**
-     * tearDownAfterClass
+     * 响应实例
      *
-     * @return void
+     * @return \Queryyetsimple\Http\Response
      */
-    public static function tearDownAfterClass()
+    public function getResponse()
     {
+        return $this->objResponse;
     }
 
     /**
-     * setUp
+     * 返回验证器
      *
-     * @return void
+     * @return \Queryyetsimple\Validate\IValidate
      */
-    protected function setUp()
+    public function getValidate()
     {
-    }
-
-    /**
-     * tearDown
-     *
-     * @return void
-     */
-    protected function tearDown()
-    {
+        return $this->objValidate;
     }
 }
