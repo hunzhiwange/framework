9de997ead48bd662728458f10e4af3e042d31d8f
Xiangmin Liu <l>
2018-03-18 09:49:13 +0800

    to a new computer update

--- Http/ServerBagTest.php
+++ Http/ServerBagTest.php
@@ -17,54 +17,41 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Tests;
+namespace Tests\Router;
 
-use PHPUnit\Framework\TestCase as TestCases;
+use Tests\TestCase;
+use Queryyetsimple\Http\ServerBag;
 
 /**
- * phpunit 测试用例
- *
+ * ServerBag test
+ * This class borrows heavily from the Symfony2 Framework and is part of the symfony package
+ * 
  * @author Xiangmin Liu <635750556@qq.com>
  * @package $$
- * @since 2017.05.09
+ * @since 2018.03.14
  * @version 1.0
+ * @see Symfony\Component\HttpFoundation (https://github.com/symfony/symfony)
  */
-abstract class TestCase extends TestCases
+class ServerBagTest extends TestCase
 {
 
-    /**
-     * setUpBeforeClass
-     *
-     * @return void
-     */
-    public static function setUpBeforeClass()
+    public function testShouldExtractHeadersFromServerArray()
     {
-    }
+        $server = array(
+            'SOME_SERVER_VARIABLE' => 'value',
+            'SOME_SERVER_VARIABLE2' => 'value',
+            'ROOT' => 'value',
+            'HTTP_CONTENT_TYPE' => 'text/html',
+            'HTTP_CONTENT_LENGTH' => '0',
+            'HTTP_ETAG' => 'asdf',
+        );
 
-    /**
-     * tearDownAfterClass
-     *
-     * @return void
-     */
-    public static function tearDownAfterClass()
-    {
-    }
+        $bag = new ServerBag($server);
 
-    /**
-     * setUp
-     *
-     * @return void
-     */
-    protected function setUp()
-    {
-    }
-
-    /**
-     * tearDown
-     *
-     * @return void
-     */
-    protected function tearDown()
-    {
+        $this->assertEquals(array(
+            'CONTENT_TYPE' => 'text/html',
+            'CONTENT_LENGTH' => '0',
+            'ETAG' => 'asdf'
+        ), $bag->getHeaders());
     }
 }
