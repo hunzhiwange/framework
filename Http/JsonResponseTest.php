bbc547296a0052a673ac2e8c720e7b3df604d0f5
Xiangmin Liu <635750556@qq.com>
2018-03-23 08:54:13 +0800

    new logo for it

--- Http/JsonResponseTest.php
+++ Http/JsonResponseTest.php
@@ -118,73 +118,85 @@ class JsonResponseTest extends TestCase
         $this->assertSame('{}', $response->getContent());
     }
 
-    public function t2estStaticCreateJsonArray()
+    public function testStaticCreateJsonArray()
     {
         $response = JsonResponse::create(array(0, 1, 2, 3));
         $this->assertInstanceOf('Queryyetsimple\Http\JsonResponse', $response);
         $this->assertSame('[0,1,2,3]', $response->getContent());
     }
 
-    public function t2estStaticCreateJsonObject()
+    public function testStaticCreateJsonObject()
     {
         $response = JsonResponse::create(array('foo' => 'bar'));
-        $this->assertInstanceOf('Symfony\Component\HttpFoundation\JsonResponse', $response);
+        $this->assertInstanceOf('Queryyetsimple\Http\JsonResponse', $response);
         $this->assertSame('{"foo":"bar"}', $response->getContent());
     }
-    public function t2estStaticCreateWithSimpleTypes()
+
+    public function testStaticCreateWithSimpleTypes()
     {
         $response = JsonResponse::create('foo');
-        $this->assertInstanceOf('Symfony\Component\HttpFoundation\JsonResponse', $response);
+        $this->assertInstanceOf('Queryyetsimple\Http\JsonResponse', $response);
         $this->assertSame('"foo"', $response->getContent());
+
         $response = JsonResponse::create(0);
-        $this->assertInstanceOf('Symfony\Component\HttpFoundation\JsonResponse', $response);
+        $this->assertInstanceOf('Queryyetsimple\Http\JsonResponse', $response);
         $this->assertSame('0', $response->getContent());
+
         $response = JsonResponse::create(0.1);
-        $this->assertInstanceOf('Symfony\Component\HttpFoundation\JsonResponse', $response);
+        $this->assertInstanceOf('Queryyetsimple\Http\JsonResponse', $response);
         $this->assertSame('0.1', $response->getContent());
+
         $response = JsonResponse::create(true);
-        $this->assertInstanceOf('Symfony\Component\HttpFoundation\JsonResponse', $response);
+        $this->assertInstanceOf('Queryyetsimple\Http\JsonResponse', $response);
         $this->assertSame('true', $response->getContent());
     }
-    public function t2estStaticCreateWithCustomStatus()
+
+    public function testStaticCreateWithCustomStatus()
     {
         $response = JsonResponse::create(array(), 202);
         $this->assertSame(202, $response->getStatusCode());
     }
-    public function t2estStaticCreateAddsContentTypeHeader()
+
+    public function testStaticCreateAddsContentTypeHeader()
     {
         $response = JsonResponse::create();
         $this->assertSame('application/json', $response->headers->get('Content-Type'));
     }
-    public function t2estStaticCreateWithCustomHeaders()
+
+    public function testStaticCreateWithCustomHeaders()
     {
         $response = JsonResponse::create(array(), 200, array('ETag' => 'foo'));
         $this->assertSame('application/json', $response->headers->get('Content-Type'));
         $this->assertSame('foo', $response->headers->get('ETag'));
     }
-    public function t2estStaticCreateWithCustomContentType()
+
+    public function testStaticCreateWithCustomContentType()
     {
         $headers = array('Content-Type' => 'application/vnd.acme.blog-v1+json');
         $response = JsonResponse::create(array(), 200, $headers);
         $this->assertSame('application/vnd.acme.blog-v1+json', $response->headers->get('Content-Type'));
     }
-    public function t2estSetCallback()
+
+    public function testSetCallback()
     {
         $response = JsonResponse::create(array('foo' => 'bar'))->setCallback('callback');
-        $this->assertEquals('/**/callback({"foo":"bar"});', $response->getContent());
+        $this->assertEquals(';callback({"foo":"bar"});', $response->getContent());
         $this->assertEquals('text/javascript', $response->headers->get('Content-Type'));
     }
-    public function t2estJsonEncodeFlags()
+
+    public function testJsonEncodeFlags()
     {
         $response = new JsonResponse('<>\'&"');
-        $this->assertEquals('"\u003C\u003E\u0027\u0026\u0022"', $response->getContent());
+        $this->assertEquals("\"<>'&\\\"\"", $response->getContent());
     }
-    public function t2estGetEncodingOptions()
+
+    public function testGetEncodingOptions()
     {
         $response = new JsonResponse();
-        $this->assertEquals(JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT, $response->getEncodingOptions());
+        $this->assertEquals(JSON_UNESCAPED_UNICODE, $response->getEncodingOptions());
     }
-    public function t2estSetEncodingOptions()
+
+    public function testSetEncodingOptions()
     {
         $response = new JsonResponse();
         $response->setData(array(array(1, 2, 3)));
@@ -192,6 +204,7 @@ class JsonResponseTest extends TestCase
         $response->setEncodingOptions(JSON_FORCE_OBJECT);
         $this->assertEquals('{"0":{"0":1,"1":2,"2":3}}', $response->getContent());
     }
+
     public function t2estItAcceptsJsonAsString()
     {
         $response = JsonResponse::fromJsonString('{"foo":"bar"}');
