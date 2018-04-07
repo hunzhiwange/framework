80ea6ee832b98ef5ed210f979dae0e61f22e5561
Xiangmin Liu <635750556@qq.com>
2018-04-07 09:46:46 +0800

    update top namespace to leevel

--- ITree.php
+++ ITree.php
@@ -14,7 +14,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Queryyetsimple\Tree;
+namespace Leevel\Tree;
 
 /**
  * ITree 接口
