f7345a298ee7a39b7d85bc6990c84e61fca37e14
Xiangmin Liu <635750556@qq.com>
2018-04-07 09:46:46 +0800

    update top namespace to leevel

--- IEncryption.php
+++ IEncryption.php
@@ -14,7 +14,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Queryyetsimple\Encryption;
+namespace Leevel\Encryption;
 
 /**
  * IEncryption 接口
