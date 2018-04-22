605cf7b388cabe167f01f11a8fb0b0e54715cda6
Xiangmin Liu <635750556@qq.com>
2018-04-22 17:14:34 +0800

    router updagte

--- Middleware/Log.php
+++ Middleware/Log.php
@@ -14,7 +14,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Leevel\Log\middleware;
+namespace Leevel\Log\Middleware;
 
 use Closure;
 use Leevel\{
