2411d7deba27a933dec870f1b4ad19389f2befa3
Xiangmin Liu <635750556@qq.com>
2018-06-18 21:33:51 +0800

    add tests file tests/Database/Query/QueryAggregateTest.php

---
+++ Database/Query/QueryAggregateTest.php
@@ -0,0 +1,279 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ************************ package.
+ * _____________                           _______________
+ *  ______/     \__  _____  ____  ______  / /_  _________
+ *   ____/ __   / / / / _ \/ __`\/ / __ \/ __ \/ __ \___
+ *    __/ / /  / /_/ /  __/ /  \  / /_/ / / / / /_/ /__
+ *      \_\ \_/\____/\___/_/   / / .___/_/ /_/ .___/
+ *         \_\                /_/_/         /_/
+ *
+ * The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+ * (c) 2010-2018 http://queryphp.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Database\Query;
+
+use Tests\TestCase;
+
+/**
+ * aggregate test.
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ *
+ * @since 2018.06.18
+ *
+ * @version 1.0
+ * @coversNothing
+ */
+class QueryAggregateTest extends TestCase
+{
+    use Query;
+
+    public function testBaseUse()
+    {
+        $connect = $this->createConnect();
+
+        $sql = <<<'eot'
+array (
+  0 => 'SELECT COUNT(*) AS row_count FROM `test` LIMIT 1',
+  1 => 
+  array (
+  ),
+  2 => false,
+  3 => NULL,
+  4 => NULL,
+  5 => 
+  array (
+  ),
+)
+eot;
+
+        $this->assertSame(
+            $sql,
+            $this->varExport(
+                $connect->table('test')->
+
+                count()->
+
+                getOne(true),
+                __METHOD__
+            )
+        );
+
+        $sql = <<<'eot'
+array (
+  0 => 'SELECT COUNT(`test`.`id`) AS row_count FROM `test` LIMIT 1',
+  1 => 
+  array (
+  ),
+  2 => false,
+  3 => NULL,
+  4 => NULL,
+  5 => 
+  array (
+  ),
+)
+eot;
+
+        $this->assertSame(
+            $sql,
+            $this->varExport(
+                $connect->table('test')->
+
+                count('id')->
+
+                getOne(true),
+                __METHOD__
+            )
+        );
+
+        $sql = <<<'eot'
+array (
+  0 => 'SELECT COUNT(`test`.`id`) AS count1 FROM `test` LIMIT 1',
+  1 => 
+  array (
+  ),
+  2 => false,
+  3 => NULL,
+  4 => NULL,
+  5 => 
+  array (
+  ),
+)
+eot;
+
+        $this->assertSame(
+            $sql,
+            $this->varExport(
+                $connect->table('test')->
+
+                count('id', 'count1')->
+
+                getOne(true),
+                __METHOD__
+            )
+        );
+
+        $sql = <<<'eot'
+array (
+  0 => 'SELECT COUNT(`test`.`id`*50) AS count1 FROM `test` LIMIT 1',
+  1 => 
+  array (
+  ),
+  2 => false,
+  3 => NULL,
+  4 => NULL,
+  5 => 
+  array (
+  ),
+)
+eot;
+
+        $this->assertSame(
+            $sql,
+            $this->varExport(
+                $connect->table('test')->
+
+                count('{[id]*50}', 'count1')->
+
+                getOne(true),
+                __METHOD__
+            )
+        );
+    }
+
+    public function testAvg()
+    {
+        $connect = $this->createConnect();
+
+        $sql = <<<'eot'
+array (
+  0 => 'SELECT AVG(`test`.`id`) AS avg_value FROM `test` LIMIT 1',
+  1 => 
+  array (
+  ),
+  2 => false,
+  3 => NULL,
+  4 => NULL,
+  5 => 
+  array (
+  ),
+)
+eot;
+
+        $this->assertSame(
+            $sql,
+            $this->varExport(
+                $connect->table('test')->
+
+                avg('id')->
+
+                getOne(true),
+                __METHOD__
+            )
+        );
+    }
+
+    public function testMax()
+    {
+        $connect = $this->createConnect();
+
+        $sql = <<<'eot'
+array (
+  0 => 'SELECT MAX(`test`.`num`) AS max_value FROM `test` LIMIT 1',
+  1 => 
+  array (
+  ),
+  2 => false,
+  3 => NULL,
+  4 => NULL,
+  5 => 
+  array (
+  ),
+)
+eot;
+
+        $this->assertSame(
+            $sql,
+            $this->varExport(
+                $connect->table('test')->
+
+                max('num')->
+
+                getOne(true),
+                __METHOD__
+            )
+        );
+    }
+
+    public function testMin()
+    {
+        $connect = $this->createConnect();
+
+        $sql = <<<'eot'
+array (
+  0 => 'SELECT MIN(`test`.`num`) AS min_value FROM `test` LIMIT 1',
+  1 => 
+  array (
+  ),
+  2 => false,
+  3 => NULL,
+  4 => NULL,
+  5 => 
+  array (
+  ),
+)
+eot;
+
+        $this->assertSame(
+            $sql,
+            $this->varExport(
+                $connect->table('test')->
+
+                min('num')->
+
+                getOne(true),
+                __METHOD__
+            )
+        );
+    }
+
+    public function testSum()
+    {
+        $connect = $this->createConnect();
+
+        $sql = <<<'eot'
+array (
+  0 => 'SELECT SUM(`test`.`num`) AS sum_value FROM `test` LIMIT 1',
+  1 => 
+  array (
+  ),
+  2 => false,
+  3 => NULL,
+  4 => NULL,
+  5 => 
+  array (
+  ),
+)
+eot;
+
+        $this->assertSame(
+            $sql,
+            $this->varExport(
+                $connect->table('test')->
+
+                sum('num')->
+
+                getOne(true),
+                __METHOD__
+            )
+        );
+    }
+}
