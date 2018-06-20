42f8668a28dde141dac627deaa685b583b76d43c
Xiangmin Liu <635750556@qq.com>
2018-06-20 12:59:15 +0800

    add tests files tests/Database/Query/QueryColumnsTest.php

---
+++ Database/Query/QueryColumnsTest.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ************************ package.
+ * _____________                           _______________
+ *  ______/     \__  _____  ____  ______  / /_  _________
+ *   ____/ __   / / / / _ \/ __`\/ / __ \/ __ \/ __ \___
+ *    __/ / /  / /_/ /  __/ /  \  / /_/ / / / / /_/ /__
+ *      \_\ \_/\____/\___/_/   / / .___/_/ /_/ .___/
+ *         \_\                /_/_/         /_/
+ *
+ * The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+ * (c) 2010-2018 http://queryphp.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Database\Query;
+
+use Tests\TestCase;
+
+/**
+ * columns test.
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ *
+ * @since 2018.06.20
+ *
+ * @version 1.0
+ * @coversNothing
+ */
+class QueryColumnsTest extends TestCase
+{
+    use Query;
+
+    public function testBaseUse()
+    {
+        $connect = $this->createConnect();
+
+        $sql = <<<'eot'
+array (
+  0 => 'SELECT `test`.*,`test`.`id`,`test`.`name`,`test`.`value` FROM `test`',
+  1 => 
+  array (
+  ),
+  2 => false,
+  3 => NULL,
+  4 => NULL,
+  5 => 
+  array (
+  ),
+)
+eot;
+
+        $this->assertSame(
+            $sql,
+            $this->varExport(
+                $connect->table('test')->
+
+                columns('id')->
+
+                columns('name,value')->
+
+                getAll(true),
+                __METHOD__
+            )
+        );
+    }
+
+    public function testSetColumns()
+    {
+        $connect = $this->createConnect();
+
+        $sql = <<<'eot'
+array (
+  0 => 'SELECT `test`.`remark` FROM `test`',
+  1 => 
+  array (
+  ),
+  2 => false,
+  3 => NULL,
+  4 => NULL,
+  5 => 
+  array (
+  ),
+)
+eot;
+
+        $this->assertSame(
+            $sql,
+            $this->varExport(
+                $connect->table('test')->
+
+                columns('id')->
+
+                columns('name,value')->
+
+                setColumns('remark')->
+
+                getAll(true),
+                __METHOD__
+            )
+        );
+    }
+}
