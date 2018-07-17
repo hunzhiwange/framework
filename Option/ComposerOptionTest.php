5ac3b753d0e1cfec626451ea5ac49f739edc9c63
Xiangmin Liu <635750556@qq.com>
2018-07-18 00:14:02 +0800

    Improving option component and add other tests file

---
+++ Option/ComposerOptionTest.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ************************ package.
+ * _____________                           _______________
+ *  ______/     \__  _____  ____  ______  / /_  _________
+ *   ____/ __   / / / / _ \/ __`\/ / __ \/ __ \/ __ \___
+ *    __/ / /  / /_/ /  __/ /  \  / /_/ / / / / /_/ /__
+ *      \_\ \_/\____/\___/_/   / / .___/_/ /_/ .___/
+ *         \_\                /_/_/         /_/
+ *
+ * The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+ * (c) 2010-2018 http://queryphp.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Option;
+
+use Leevel\Option\ComposerOption;
+use Tests\TestCase;
+
+/**
+ * composerOption test.
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ *
+ * @since 2018.07.17
+ *
+ * @version 1.0
+ */
+class ComposerOptionTest extends TestCase
+{
+    public function testBaseUse()
+    {
+        $options = (new ComposerOption(__DIR__.'/app1'))->loadData();
+
+        $data = <<<'eot'
+array (
+  'providers' => 
+  array (
+    0 => 'Leevel\\Auth\\Provider\\Register',
+    1 => 'Leevel\\Cache\\Provider\\Register',
+    2 => 'Leevel\\Cookie\\Provider\\Register',
+    3 => 'Leevel\\Database\\Provider\\Register',
+    4 => 'Leevel\\Encryption\\Provider\\Register',
+    5 => 'Leevel\\Filesystem\\Provider\\Register',
+    6 => 'Leevel\\Mail\\Provider\\Register',
+    7 => 'Leevel\\Mvc\\Provider\\Register',
+    8 => 'Leevel\\Page\\Provider\\Register',
+    9 => 'Leevel\\Queue\\Provider\\Register',
+    10 => 'Leevel\\Session\\Provider\\Register',
+    11 => 'Leevel\\Protocol\\Provider\\Register',
+    12 => 'Leevel\\Throttler\\Provider\\Register',
+    13 => 'Leevel\\Validate\\Provider\\Register',
+    14 => 'Leevel\\View\\Provider\\Register',
+    15 => 'Demo\\Provider\\Register',
+    16 => 'Common\\Infra\\Provider\\Event',
+    17 => 'Common\\Infra\\Provider\\Router',
+  ),
+  'ignores' => 
+  array (
+    0 => 'Leevel\\Notexits\\Provider\\Register',
+  ),
+  'commands' => 
+  array (
+    0 => 'Leevel\\Database\\Console',
+    1 => 'Leevel\\I18n\\Console',
+    2 => 'Leevel\\Mvc\\Console',
+    3 => 'Leevel\\Queue\\Console',
+    4 => 'Leevel\\Router\\Console',
+    5 => 'Leevel\\Protocol\\Console',
+    6 => 'Leevel\\Option\\Console',
+    7 => 'Demo\\Demo\\Console',
+    8 => 'Common\\App\\Console',
+  ),
+  'options' => 
+  array (
+    'demo' => 'option/extend/test.php',
+  ),
+  'i18ns' => 
+  array (
+    0 => 'i18n/extend',
+  ),
+  'metas' => 
+  array (
+    'foo' => 'bar',
+  ),
+)
+eot;
+
+        $this->assertSame(
+            $data,
+            $this->varExport(
+                $options
+            )
+        );
+    }
+}
---
+++ Option/Console/CacheTest.php
@@ -0,0 +1,206 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ************************ package.
+ * _____________                           _______________
+ *  ______/     \__  _____  ____  ______  / /_  _________
+ *   ____/ __   / / / / _ \/ __`\/ / __ \/ __ \/ __ \___
+ *    __/ / /  / /_/ /  __/ /  \  / /_/ / / / / /_/ /__
+ *      \_\ \_/\____/\___/_/   / / .___/_/ /_/ .___/
+ *         \_\                /_/_/         /_/
+ *
+ * The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+ * (c) 2010-2018 http://queryphp.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Option\Console;
+
+use Leevel\Kernel\IProject;
+use Leevel\Option\Console\Cache;
+use Leevel\Support\Facade;
+use Tests\Console\BaseCommand;
+use Tests\TestCase;
+
+/**
+ * cache test.
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ *
+ * @since 2018.07.17
+ *
+ * @version 1.0
+ */
+class CacheTest extends TestCase
+{
+    use BaseCommand;
+
+    public function testBaseUse()
+    {
+        $cacheFile = __DIR__.'/option_cache.php';
+
+        $result = $this->runCommand(
+            new Cache(),
+            [
+                'command'     => 'option:cache',
+            ],
+            function ($container) use ($cacheFile) {
+                // 静态属性会保持住，可能受到其它单元测试的影响
+                Facade::remove('option');
+
+                $container->singleton('option', function () {
+                    return new OptionService();
+                });
+
+                $project = $this->createMock(IProject::class);
+
+                $this->assertInstanceof(IProject::class, $project);
+
+                $project->method('pathCacheOptionFile')->willReturn($cacheFile);
+                $this->assertEquals($cacheFile, $project->pathCacheOptionFile());
+
+                $container->singleton(IProject::class, $project);
+            }
+        );
+
+        $this->assertContains(sprintf('Option file %s cache successed.', $cacheFile), $result);
+
+        $this->assertSame(['foo' => 'bar', 'hello' => 'world'], (array) (include $cacheFile));
+
+        unlink($cacheFile);
+    }
+
+    public function testExists()
+    {
+        $cacheFile = __DIR__.'/option_cache2.php';
+
+        file_put_contents($cacheFile, 'hello');
+
+        $this->assertTrue(is_file($cacheFile));
+        $this->assertSame('hello', file_get_contents($cacheFile));
+
+        $result = $this->runCommand(
+            new Cache(),
+            [
+                'command'     => 'option:cache',
+            ],
+            function ($container) use ($cacheFile) {
+                // 静态属性会保持住，可能受到其它单元测试的影响
+                Facade::remove('option');
+
+                $container->singleton('option', function () {
+                    return new OptionService();
+                });
+
+                $project = $this->createMock(IProject::class);
+
+                $this->assertInstanceof(IProject::class, $project);
+
+                $project->method('pathCacheOptionFile')->willReturn($cacheFile);
+                $this->assertEquals($cacheFile, $project->pathCacheOptionFile());
+
+                $container->singleton(IProject::class, $project);
+            }
+        );
+
+        $this->assertContains(sprintf('Option file %s cache successed.', $cacheFile), $result);
+
+        // 如果换成文件存在，则包含警告信息
+        $this->assertContains(sprintf('Option cache file %s is already exits.', $cacheFile), $result);
+
+        $this->assertSame(['foo' => 'bar', 'hello' => 'world'], (array) (include $cacheFile));
+
+        unlink($cacheFile);
+    }
+
+    public function testDirNotExists()
+    {
+        $cacheFile = __DIR__.'/dirNotExists/option_cache.php';
+
+        $result = $this->runCommand(
+            new Cache(),
+            [
+                'command'     => 'option:cache',
+            ],
+            function ($container) use ($cacheFile) {
+                // 静态属性会保持住，可能受到其它单元测试的影响
+                Facade::remove('option');
+
+                $container->singleton('option', function () {
+                    return new OptionService();
+                });
+
+                $project = $this->createMock(IProject::class);
+
+                $this->assertInstanceof(IProject::class, $project);
+
+                $project->method('pathCacheOptionFile')->willReturn($cacheFile);
+                $this->assertEquals($cacheFile, $project->pathCacheOptionFile());
+
+                $container->singleton(IProject::class, $project);
+            }
+        );
+
+        $this->assertContains(sprintf('Option file %s cache successed.', $cacheFile), $result);
+
+        $this->assertSame(['foo' => 'bar', 'hello' => 'world'], (array) (include $cacheFile));
+
+        unlink($cacheFile);
+        rmdir(dirname($cacheFile));
+    }
+
+    public function testDirWriteable()
+    {
+        $dirname = __DIR__.'/dirWriteable';
+        $cacheFile = $dirname.'/option_cache.php';
+
+        // 设置目录只读
+        // 7 = 4+2+1 分别代表可读可写可执行
+        mkdir($dirname, 0444);
+
+        $this->assertDirectoryExists($dirname);
+
+        $result = $this->runCommand(
+            new Cache(),
+            [
+                'command'     => 'option:cache',
+            ],
+            function ($container) use ($cacheFile) {
+                // 静态属性会保持住，可能受到其它单元测试的影响
+                Facade::remove('option');
+
+                $container->singleton('option', function () {
+                    return new OptionService();
+                });
+
+                $project = $this->createMock(IProject::class);
+
+                $this->assertInstanceof(IProject::class, $project);
+
+                $project->method('pathCacheOptionFile')->willReturn($cacheFile);
+                $this->assertEquals($cacheFile, $project->pathCacheOptionFile());
+
+                $container->singleton(IProject::class, $project);
+            }
+        );
+
+        $this->assertContains(sprintf('Dir %s is not', $dirname), $result);
+
+        rmdir($dirname);
+    }
+}
+
+class OptionService
+{
+    public function all()
+    {
+        return [
+            'foo'   => 'bar',
+            'hello' => 'world',
+        ];
+    }
+}
