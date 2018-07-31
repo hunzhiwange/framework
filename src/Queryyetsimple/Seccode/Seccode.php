9ea624779894d0faa9434e8b18e70f2b0fef7a74
Xiangmin Liu <635750556@qq.com>
2018-07-31 23:57:32 +0800

    test4

--- src/Queryyetsimple/Seccode/Seccode.php
+++ src/Queryyetsimple/Seccode/Seccode.php
@@ -134,7 +134,7 @@ class Seccode implements ISeccode
         $resImage = imagecreatefromstring($this->makeBackground());
 
         if ($this->option['adulterate']) {
-            //$this->makeAdulterate($resImage);
+            $this->makeAdulterate($resImage);
         }
 
         $this->makeTtfFont($resImage);
@@ -531,7 +531,7 @@ class Seccode implements ISeccode
                 );
 
                 imagedestroy($resBackground);
-var_dump($color);
+
                 $findBackground = true;
 
                 $this->fontColor = $color;
