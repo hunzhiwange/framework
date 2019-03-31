48e859a80b216a8988460464551e5339baee9fc7
Xiangmin Liu <635750556@qq.com>
2019-03-31 12:25:59 +0800

    fix: fix the namespace of Router\ViewTest

--- tests/Router/ViewTest.php
+++ tests/Router/ViewTest.php
@@ -18,7 +18,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Tests\Mvc;
+namespace Tests\Router;
 
 use Leevel\Router\IView;
 use Leevel\Router\View;
