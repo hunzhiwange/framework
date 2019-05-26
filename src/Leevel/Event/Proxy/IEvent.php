a6858879cbdf195561df959610fe535536e300d9
XiangMin Liu <635750556@qq.com>
2019-05-26 14:45:43 +0800

    refactor(event proxy): event proxy better for ide

--- src/Leevel/Event/Proxy/IEvent.php
+++ src/Leevel/Event/Proxy/IEvent.php
@@ -20,44 +20,26 @@ declare(strict_types=1);
 
 namespace Leevel\Event\Proxy;
 
-use Leevel\Di\Container;
-use Leevel\Event\Dispatch;
-
 /**
- * 代理 event.
+ * 代理 event 接口.
  *
  * @author Xiangmin Liu <635750556@qq.com>
  *
- * @since 2017.07.12
+ * @since 2019.05.26
  *
  * @version 1.0
- * @codeCoverageIgnore
+ *
+ * @see \Leevel\Event\IDispatch 请保持接口设计的一致
  */
-class Event implements IEvent
+interface IEvent
 {
-    /**
-     * call.
-     *
-     * @param string $method
-     * @param array  $args
-     *
-     * @return mixed
-     */
-    public static function __callStatic(string $method, array $args)
-    {
-        return self::proxy()->{$method}(...$args);
-    }
-
     /**
      * 执行一个事件.
      *
      * @param object|string $event
      * @param array         ...$params
      */
-    public static function handle($event, ...$params): void
-    {
-        self::proxy()->handle($event, ...$params);
-    }
+    public static function handle($event, ...$params): void;
 
     /**
      * 注册监听器.
@@ -66,10 +48,7 @@ class Event implements IEvent
      * @param mixed               $listener
      * @param int                 $priority
      */
-    public static function register($event, $listener, int $priority = 500): void
-    {
-        self::proxy()->register($event, $listener, $priority);
-    }
+    public static function register($event, $listener, int $priority = 500): void;
 
     /**
      * 获取一个事件监听器.
@@ -78,10 +57,7 @@ class Event implements IEvent
      *
      * @return array
      */
-    public static function get($event): array
-    {
-        return self::proxy()->get($event);
-    }
+    public static function get($event): array;
 
     /**
      * 判断事件监听器是否存在.
@@ -90,28 +66,12 @@ class Event implements IEvent
      *
      * @return bool
      */
-    public static function has($event): bool
-    {
-        return self::proxy()->has($event);
-    }
+    public static function has($event): bool;
 
     /**
      * 删除一个事件所有监听器.
      *
      * @param object|string $event
      */
-    public static function delete($event): void
-    {
-        self::proxy()->delete($event);
-    }
-
-    /**
-     * 代理服务
-     *
-     * @return \Leevel\Event\Dispatch
-     */
-    public static function proxy(): Dispatch
-    {
-        return Container::singletons()->make('event');
-    }
+    public static function delete($event): void;
 }
