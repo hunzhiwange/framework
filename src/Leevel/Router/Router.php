975ed36ec6445accfa5fa25319301db1d53a2f47
XiangMin Liu <635750556@qq.com>
2019-06-13 21:53:45 +0800

    refactor(style): code poem

--- src/Leevel/Router/Router.php
+++ src/Leevel/Router/Router.php
@@ -397,7 +397,10 @@ class Router implements IRouter
      */
     protected function resolveMatchedData(array $data): void
     {
-        $data = $this->mergeMatchedData($data, $this->preRequestMatched[spl_object_id($this->request)] ?? []);
+        $data = $this->mergeMatchedData(
+            $data,
+            $this->preRequestMatched[spl_object_id($this->request)] ?? [],
+        );
 
         if (!$data[IRouter::APP]) {
             $data[IRouter::APP] = self::DEFAULT_APP;
