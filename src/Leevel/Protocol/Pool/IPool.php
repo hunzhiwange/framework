fdd133597b8569a191d535248a0650989932ad8f
XiangMin Liu <635750556@qq.com>
2019-07-07 19:38:42 +0800

    feat: add pool for swoole

---
+++ src/Leevel/Protocol/Pool/IPool.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ************************ package.
+ * _____________                           _______________
+ *  ______/     \__  _____  ____  ______  / /_  _________
+ *   ____/ __   / / / / _ \/ __`\/ / __ \/ __ \/ __ \___
+ *    __/ / /  / /_/ /  __/ /  \  / /_/ / / / / /_/ /__
+ *      \_\ \_/\____/\___/_/   / / .___/_/ /_/ .___/
+ *         \_\                /_/_/         /_/
+ *
+ * The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+ * (c) 2010-2019 http://queryphp.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Leevel\Protocol\Pool;
+
+/**
+ * 连接池接口.
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ *
+ * @since 2019.07.05
+ *
+ * @version 1.0
+ * @codeCoverageIgnore
+ */
+interface IPool
+{
+    /**
+     * 连接最后活跃时间.
+     *
+     * @var string
+     */
+    const LAST_ACTIVE_TIME = 'LAST_ACTIVE_TIME';
+}
