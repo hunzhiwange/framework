84085c2b2517caef2efe9f3e036a6ac003cedc16
XiangMin Liu <635750556@qq.com>
2019-08-10 22:15:49 +0800

    test(kernel): add tests for kernel

---
+++ tests/Kernel/HelperTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ************************ package.
+ * _____________                           _______________
+ *  ______/     \__  _____  ____  ______  / /_  _________
+ *   ____/ __   / / / / _ \/ __`\/ / __ \/ __ \/ __ \___
+ *    __/ / /  / /_/ /  __/ /  \  / /_/ / / / / /_/ /__
+ *      \_\ \_/\____/\___/_/   / / .___/_/ /_/ .___/
+ *         \_\                /_/_/         /_/
+ *
+ * The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+ * (c) 2010-2019 http://queryphp.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Kernel;
+
+use Leevel\Di\Container;
+use Leevel\Di\IContainer;
+use Leevel\Kernel\App;
+use Tests\TestCase;
+
+/**
+ * helper test.
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ *
+ * @since 2019.08.10
+ *
+ * @version 1.0
+ */
+class HelperTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->tearDown();
+    }
+
+    protected function tearDown(): void
+    {
+        Container::singletons()->clear();
+    }
+
+    public function testApp(): void
+    {
+        $container = $this->createContainer();
+        $app = f('Leevel\\Kernel\\Helper\\app');
+        $container->alias([
+            'app' => [
+                'Leevel\\Leevel\\App',
+                'Leevel\\Kernel\\IApp',
+            ],
+        ]);
+
+        $this->assertInstanceof(IContainer::class, $container);
+        $this->assertInstanceof(Container::class, $container);
+        $this->assertInstanceof(App::class, $app);
+        $this->assertInstanceof(App::class, $container->make('app'));
+        $this->assertSame('fooNotFound', $container->make('fooNotFound'));
+    }
+
+    protected function createContainer(): Container
+    {
+        $container = Container::singletons();
+        $container->clear();
+        $container->instance('app', new App($container, ''));
+
+        return $container;
+    }
+}
