ee79b54f42e6ba5e38513fd60f4d3321b84060b7
XiangMin Liu <635750556@qq.com>
2019-08-10 19:48:19 +0800

    test(router): add helper func for router

---
+++ tests/Router/HelperTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ************************ package.
+ * _____________                           _______________
+ *  ______/     \__  _____  ____  ______  / /_  _________
+ *   ____/ __   / / / / _ \/ __`\/ / __ \/ __ \/ __ \___
+ *    __/ / /  / /_/ /  __/ /  \  / /_/ / / / / /_/ /__
+ *      \_\ \_/\____/\___/_/   / / .___/_/ /_/ .___/
+ *         \_\                /_/_/         /_/
+ *
+ * The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+ * (c) 2010-2019 http://queryphp.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Router;
+
+use Leevel\Di\Container;
+use Leevel\Router\IUrl;
+use Tests\TestCase;
+
+/**
+ * helper test.
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ *
+ * @since 2019.08.10
+ *
+ * @version 1.0
+ */
+class HelperTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->tearDown();
+    }
+
+    protected function tearDown(): void
+    {
+        Container::singletons()->clear();
+    }
+
+    public function testUrl(): void
+    {
+        $url = $this->createMock(IUrl::class);
+        $url->method('make')->willReturn('/goods?foo=bar');
+        $this->assertSame('/goods?foo=bar', $url->make('/goods', ['foo' => 'bar']));
+
+        $container = $this->createContainer();
+        $container->singleton('url', function () use ($url) {
+            return $url;
+        });
+
+        $this->assertSame('/goods?foo=bar', f('Leevel\\Router\\Helper\\url', '/goods', ['foo' => 'bar']));
+    }
+
+    protected function createContainer(): Container
+    {
+        $container = Container::singletons();
+        $container->clear();
+
+        return $container;
+    }
+}
