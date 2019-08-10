201739c6cce72459800c25046bca69a60a0b5cd0
XiangMin Liu <635750556@qq.com>
2019-08-10 19:02:30 +0800

    test(session): add helper func for session

---
+++ tests/Session/HelperTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ************************ package.
+ * _____________                           _______________
+ *  ______/     \__  _____  ____  ______  / /_  _________
+ *   ____/ __   / / / / _ \/ __`\/ / __ \/ __ \/ __ \___
+ *    __/ / /  / /_/ /  __/ /  \  / /_/ / / / / /_/ /__
+ *      \_\ \_/\____/\___/_/   / / .___/_/ /_/ .___/
+ *         \_\                /_/_/         /_/
+ *
+ * The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+ * (c) 2010-2019 http://queryphp.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Session;
+
+use Leevel\Di\Container;
+use Leevel\Session\ISession;
+use Tests\TestCase;
+
+/**
+ * helper test.
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ *
+ * @since 2019.08.10
+ *
+ * @version 1.0
+ */
+class HelperTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->tearDown();
+    }
+
+    protected function tearDown(): void
+    {
+        Container::singletons()->clear();
+    }
+
+    public function testSession(): void
+    {
+        $session = $this->createMock(ISession::class);
+        $this->assertNull($session->set('foo', 'bar'));
+        $session->method('get')->willReturn('bar');
+        $this->assertSame('bar', $session->get('foo'));
+
+        $container = $this->createContainer();
+        $container->singleton('sessions', function () use ($session) {
+            return $session;
+        });
+
+        $this->assertInstanceof(ISession::class, f('Leevel\\Session\\Helper\\session'));
+        $this->assertNull(f('Leevel\\Session\\Helper\\session_set', 'foo', 'bar'));
+        $this->assertSame('bar', f('Leevel\\Session\\Helper\\session_get', 'foo'));
+    }
+
+    protected function createContainer(): Container
+    {
+        $container = Container::singletons();
+        $container->clear();
+
+        return $container;
+    }
+}
