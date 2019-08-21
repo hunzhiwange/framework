925338a43c23454783635277cd02b2a0a290b354
XiangMin Liu <635750556@qq.com>
2019-08-21 22:31:09 +0800

    tests(validate): add tests for Leevel\Validate\Helper

---
+++ tests/Validate/HelperTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ************************ package.
+ * _____________                           _______________
+ *  ______/     \__  _____  ____  ______  / /_  _________
+ *   ____/ __   / / / / _ \/ __`\/ / __ \/ __ \/ __ \___
+ *    __/ / /  / /_/ /  __/ /  \  / /_/ / / / / /_/ /__
+ *      \_\ \_/\____/\___/_/   / / .___/_/ /_/ .___/
+ *         \_\                /_/_/         /_/
+ *
+ * The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+ * (c) 2010-2019 http://queryphp.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Validate;
+
+use Leevel\Validate\Helper;
+use Tests\TestCase;
+
+/**
+ * validator test.
+ *
+ * @author Xiangmin Liu <635750556@qq.com>
+ *
+ * @since 2019.08.21
+ *
+ * @version 1.0
+ */
+class HelperTest extends TestCase
+{
+    public function testBaseUse(): void
+    {
+        $this->assertTrue(Helper::validateRequired(5));
+        $this->assertTrue(Helper::validateRequired(0));
+        $this->assertFalse(Helper::validateRequired(''));
+    }
+
+    public function testBaseUse2(): void
+    {
+        $this->expectException(\Error::class);
+        $this->expectExceptionMessage(
+            'Call to undefined function Leevel\\Validate\\Helper\\not_found()'
+        );
+
+        $this->assertFalse(Helper::notFound());
+    }
+}
