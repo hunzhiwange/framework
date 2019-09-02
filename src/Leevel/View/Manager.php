5424ce1cd78e0e14bd1b93c93e50d70e36087af4
XiangMin Liu <635750556@qq.com>
2019-09-02 22:08:34 +0800

    refactor(view): normalize proxy for view

--- src/Leevel/View/Manager.php
+++ src/Leevel/View/Manager.php
@@ -30,11 +30,18 @@ use Leevel\Manager\Manager as Managers;
  * @since 2018.01.10
  *
  * @version 1.0
+ *
+ * @method static display(string $file, array $vars = [], ?string $ext = null, bool $display = true) 加载视图文件.
+ * @method static void setParseResolver(\Closure $parseResolver)                                     设置 parser 解析回调.
+ * @method static string getCachePath(string $file)                                                  获取编译路径.
+ * @method static \Leevel\View\IView setOption(string $name, $value)                                 设置配置.
+ * @method static void setVar($name, $value = null)                                                  设置模板变量.
+ * @method static getVar(?string $name = null)                                                       获取变量值.
+ * @method static void deleteVar(array $name)                                                        删除变量值.
+ * @method static void clearVar()                                                                    清空变量值.
  */
-class Manager extends Managers implements IView
+class Manager extends Managers
 {
-    use Proxy;
-
     /**
      * 取得配置命名空间.
      *
