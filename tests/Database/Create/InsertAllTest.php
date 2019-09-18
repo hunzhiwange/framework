a7176802cd6090a786700716b083422857212286
XiangMin Liu <635750556@qq.com>
2019-09-18 08:36:25 +0800

    tests(database): add tests for insertAll empty data

--- tests/Database/Create/InsertAllTest.php
+++ tests/Database/Create/InsertAllTest.php
@@ -374,4 +374,62 @@ class InsertAllTest extends TestCase
             ->table('test')
             ->insertAll($data);
     }
+
+    public function testInsertWithEmptyData(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
+
+        $sql = <<<'eot'
+            [
+                "INSERT INTO `test` () VALUES (),(),(),()",
+                []
+            ]
+            eot;
+
+        $data = [
+            [],
+            [],
+            [],
+            [],
+        ];
+
+        $this->assertSame(
+            $sql,
+            $this->varJson(
+                $connect
+                    ->sql()
+                    ->table('test')
+                    ->insertAll($data)
+            )
+        );
+    }
+
+    public function testReplaceWithEmptyData(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
+
+        $sql = <<<'eot'
+            [
+                "REPLACE INTO `test` () VALUES (),(),(),()",
+                []
+            ]
+            eot;
+
+        $data = [
+            [],
+            [],
+            [],
+            [],
+        ];
+
+        $this->assertSame(
+            $sql,
+            $this->varJson(
+                $connect
+                    ->sql()
+                    ->table('test')
+                    ->insertAll($data, [], true)
+            )
+        );
+    }
 }
