25192178124a0bea524d797fffca44514404d937
XiangMin Liu <635750556@qq.com>
2019-09-22 15:48:14 +0800

    tests(entity): add tests for entity event

--- tests/Database/Ddd/EntityEventTest.php
+++ tests/Database/Ddd/EntityEventTest.php
@@ -38,20 +38,22 @@ use Tests\Database\Ddd\Entity\TestEventEntity;
  */
 class EntityEventTest extends TestCase
 {
+    protected function tearDown(): void
+    {
+        Entity::withEventDispatch(null);
+    }
+
     public function testBaseUse(): void
     {
         $dispatch = new Dispatch(new Container());
-
         $this->assertNull(Entity::eventDispatch());
         Entity::withEventDispatch($dispatch);
         $this->assertInstanceof(Dispatch::class, Entity::eventDispatch());
 
         $test = new TestEventEntity(['name' => 'foo']);
-
         TestEventEntity::event(IEntity::BEFORE_CREATE_EVENT, function () {
             $_SERVER['ENTITY.BEFORE_CREATE_EVENT'] = 'BEFORE_CREATE_EVENT';
         });
-
         TestEventEntity::event(IEntity::AFTER_CREATE_EVENT, function () {
             $_SERVER['ENTITY.AFTER_CREATE_EVENT'] = 'AFTER_CREATE_EVENT';
         });
@@ -67,8 +69,63 @@ class EntityEventTest extends TestCase
         $this->assertSame('AFTER_CREATE_EVENT', $_SERVER['ENTITY.AFTER_CREATE_EVENT']);
 
         unset($_SERVER['ENTITY.BEFORE_CREATE_EVENT'], $_SERVER['ENTITY.AFTER_CREATE_EVENT']);
+    }
 
-        Entity::withEventDispatch(null);
+    public function testEventDispatchWasNotSet(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Event dispatch was not set.');
+
+        TestEventEntity::event(IEntity::BEFORE_CREATE_EVENT, function () {
+        });
+    }
+
+    /**
+     * @dataProvider getSupportedEvent
+     *
+     * @param string $event
+     */
+    public function testSupportEvent($event): void
+    {
+        $dispatch = new Dispatch(new Container());
+        $this->assertNull(Entity::eventDispatch());
+        Entity::withEventDispatch($dispatch);
+        $this->assertInstanceof(Dispatch::class, Entity::eventDispatch());
+
+        $supportEvent = TestEventEntity::supportEvent();
+        $this->assertTrue(in_array($event, $supportEvent, true));
+    }
+
+    public function getSupportedEvent()
+    {
+        return [
+            [IEntity::BEFORE_SAVE_EVENT],
+            [IEntity::AFTER_SAVE_EVENT],
+            [IEntity::BEFORE_CREATE_EVENT],
+            [IEntity::AFTER_CREATE_EVENT],
+            [IEntity::BEFORE_UPDATE_EVENT],
+            [IEntity::AFTER_UPDATE_EVENT],
+            [IEntity::BEFORE_DELETE_EVENT],
+            [IEntity::AFTER_DELETE_EVENT],
+            [IEntity::BEFORE_SOFT_DELETE_EVENT],
+            [IEntity::AFTER_SOFT_DELETE_EVENT],
+            [IEntity::BEFORE_SOFT_RESTORE_EVENT],
+            [IEntity::AFTER_SOFT_RESTORE_EVENT],
+        ];
+    }
+
+    public function testEventWasNotSupport(): void
+    {
+        $dispatch = new Dispatch(new Container());
+        $this->assertNull(Entity::eventDispatch());
+        Entity::withEventDispatch($dispatch);
+        $this->assertInstanceof(Dispatch::class, Entity::eventDispatch());
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Event `not_support` do not support.');
+
+        TestEventEntity::event('not_support', function () {
+        });
     }
 
     protected function getDatabaseTable(): array
