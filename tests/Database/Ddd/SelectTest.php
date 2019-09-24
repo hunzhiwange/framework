319004a7664efe8e21d813abefec4cbe7844e5b5
XiangMin Liu <635750556@qq.com>
2019-09-24 21:51:31 +0800

    tests(select): add tests for Leevel\Database\Ddd\Select

--- tests/Database/Ddd/SelectTest.php
+++ tests/Database/Ddd/SelectTest.php
@@ -20,9 +20,11 @@ declare(strict_types=1);
 
 namespace Tests\Database\Ddd;
 
+use Exception;
 use Leevel\Collection\Collection;
 use Leevel\Database\Ddd\IEntity;
 use Leevel\Database\Ddd\Select;
+use Leevel\Database\Page;
 use Tests\Database\DatabaseTestCase as TestCase;
 use Tests\Database\Ddd\Entity\Relation\Post;
 
@@ -188,7 +190,7 @@ class SelectTest extends TestCase
         $this->assertCount(0, $posts);
     }
 
-    public function testWithoutSoftDeleted(): void
+    public function testEntityDefaultWithoutSoftDeleted(): void
     {
         $connect = $this->createDatabaseConnect();
 
@@ -215,8 +217,14 @@ class SelectTest extends TestCase
                 ]));
 
         $select = new Select($post = Post::select()->findEntity(1));
-
         $posts = $select->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
 
         $this->assertInstanceof(Collection::class, $posts);
         $this->assertCount(2, $posts);
@@ -226,14 +234,232 @@ class SelectTest extends TestCase
         $this->assertFalse($post->softDeleted());
 
         $posts = $select->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
 
         $this->assertInstanceof(Collection::class, $posts);
         $this->assertCount(1, $posts);
 
         $posts = Post::select()->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
 
         $this->assertInstanceof(Collection::class, $posts);
         $this->assertCount(1, $posts);
+    }
+
+    public function testEntityWithSoftDeleted(): void
+    {
+        $connect = $this->createDatabaseConnect();
+
+        $this->assertSame(
+            1,
+            $connect
+                ->table('post')
+                ->insert([
+                    'title'     => 'hello world',
+                    'user_id'   => 1,
+                    'summary'   => 'post summary',
+                    'delete_at' => 0,
+                ]));
+
+        $this->assertSame(
+            2,
+            $connect
+                ->table('post')
+                ->insert([
+                    'title'     => 'hello world',
+                    'user_id'   => 1,
+                    'summary'   => 'post summary',
+                    'delete_at' => 0,
+                ]));
+
+        $select = new Select($post = Post::select()->findEntity(1));
+        $posts = $select->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(2, $posts);
+
+        $this->assertFalse($post->softDeleted());
+        $this->assertSame(1, Post::softDestroy([1]));
+        $this->assertFalse($post->softDeleted());
+
+        $posts = $select->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(1, $posts);
+
+        $posts = Post::withSoftDeleted()->findAll();
+        $sql = <<<'eot'
+            SQL: [27] SELECT `post`.* FROM `post` | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(2, $posts);
+    }
+
+    public function testEntityOnlySoftDeleted(): void
+    {
+        $connect = $this->createDatabaseConnect();
+
+        $this->assertSame(
+            1,
+            $connect
+                ->table('post')
+                ->insert([
+                    'title'     => 'hello world',
+                    'user_id'   => 1,
+                    'summary'   => 'post summary',
+                    'delete_at' => 0,
+                ]));
+
+        $this->assertSame(
+            2,
+            $connect
+                ->table('post')
+                ->insert([
+                    'title'     => 'hello world',
+                    'user_id'   => 1,
+                    'summary'   => 'post summary',
+                    'delete_at' => 0,
+                ]));
+
+        $select = new Select($post = Post::select()->findEntity(1));
+        $posts = $select->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(2, $posts);
+
+        $this->assertFalse($post->softDeleted());
+        $this->assertSame(1, Post::softDestroy([1]));
+        $this->assertFalse($post->softDeleted());
+
+        $posts = $select->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(1, $posts);
+
+        $posts = Post::onlySoftDeleted()->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` > 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(1, $posts);
+    }
+
+    public function testWithSoftDeleted(): void
+    {
+        $connect = $this->createDatabaseConnect();
+
+        $this->assertSame(
+            1,
+            $connect
+                ->table('post')
+                ->insert([
+                    'title'     => 'hello world',
+                    'user_id'   => 1,
+                    'summary'   => 'post summary',
+                    'delete_at' => 0,
+                ]));
+
+        $this->assertSame(
+            2,
+            $connect
+                ->table('post')
+                ->insert([
+                    'title'     => 'hello world',
+                    'user_id'   => 1,
+                    'summary'   => 'post summary',
+                    'delete_at' => 0,
+                ]));
+
+        $select = new Select($post = Post::select()->findEntity(1));
+        $posts = $select->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(2, $posts);
+
+        $this->assertFalse($post->softDeleted());
+        $this->assertSame(1, Post::softDestroy([1]));
+        $this->assertFalse($post->softDeleted());
+
+        $posts = $select->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(1, $posts);
+
+        $posts = $select->withSoftDeleted()->findAll();
+        $sql = <<<'eot'
+            SQL: [27] SELECT `post`.* FROM `post` | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(2, $posts);
     }
 
     public function testOnlySoftDeleted(): void
@@ -263,8 +489,14 @@ class SelectTest extends TestCase
                 ]));
 
         $select = new Select($post = Post::select()->findEntity(1));
-
         $posts = $select->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
 
         $this->assertInstanceof(Collection::class, $posts);
         $this->assertCount(2, $posts);
@@ -274,14 +506,156 @@ class SelectTest extends TestCase
         $this->assertFalse($post->softDeleted());
 
         $posts = $select->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
 
         $this->assertInstanceof(Collection::class, $posts);
         $this->assertCount(1, $posts);
 
-        $posts = Post::onlySoftDeleted()->findAll();
+        $posts = $select->onlySoftDeleted()->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` > 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
 
         $this->assertInstanceof(Collection::class, $posts);
         $this->assertCount(1, $posts);
+    }
+
+    public function testWithSoftDeletedWillInitSelect(): void
+    {
+        $connect = $this->createDatabaseConnect();
+
+        $this->assertSame(
+            1,
+            $connect
+                ->table('post')
+                ->insert([
+                    'title'     => 'hello world',
+                    'user_id'   => 1,
+                    'summary'   => 'post summary',
+                    'delete_at' => 0,
+                ]));
+
+        $this->assertSame(
+            2,
+            $connect
+                ->table('post')
+                ->insert([
+                    'title'     => 'hello world',
+                    'user_id'   => 1,
+                    'summary'   => 'post summary',
+                    'delete_at' => 0,
+                ]));
+
+        $select = new Select($post = Post::select()->findEntity(1));
+        $posts = $select->where('id', '>', 1)->findAll();
+        $sql = <<<'eot'
+            SQL: [76] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 AND `post`.`id` > 1 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(1, $posts);
+
+        $posts = $select->where('id', '>', 2)->findAll();
+        $sql = <<<'eot'
+            SQL: [96] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 AND `post`.`id` > 1 AND `post`.`id` > 2 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(0, $posts);
+
+        $posts = $select->withSoftDeleted()->findAll();
+        $sql = <<<'eot'
+            SQL: [27] SELECT `post`.* FROM `post` | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(2, $posts);
+    }
+
+    public function testOnlySoftDeletedWillInitSelect(): void
+    {
+        $connect = $this->createDatabaseConnect();
+
+        $this->assertSame(
+            1,
+            $connect
+                ->table('post')
+                ->insert([
+                    'title'     => 'hello world',
+                    'user_id'   => 1,
+                    'summary'   => 'post summary',
+                    'delete_at' => 0,
+                ]));
+
+        $this->assertSame(
+            2,
+            $connect
+                ->table('post')
+                ->insert([
+                    'title'     => 'hello world',
+                    'user_id'   => 1,
+                    'summary'   => 'post summary',
+                    'delete_at' => 0,
+                ]));
+
+        $select = new Select($post = Post::select()->findEntity(1));
+        $posts = $select->where('id', '>', 1)->findAll();
+        $sql = <<<'eot'
+            SQL: [76] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 AND `post`.`id` > 1 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(1, $posts);
+
+        $posts = $select->where('id', '>', 2)->findAll();
+        $sql = <<<'eot'
+            SQL: [96] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` = 0 AND `post`.`id` > 1 AND `post`.`id` > 2 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(0, $posts);
+
+        $posts = $select->onlySoftDeleted()->findAll();
+        $sql = <<<'eot'
+            SQL: [56] SELECT `post`.* FROM `post` WHERE `post`.`delete_at` > 0 | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Collection::class, $posts);
+        $this->assertCount(0, $posts);
     }
 
     public function testLastSql(): void
@@ -315,6 +689,110 @@ class SelectTest extends TestCase
         $this->assertSame(
             $sql,
             $select->databaseConnect()->getLastSql(),
+        );
+    }
+
+    public function testWithoutPreLoadsResult(): void
+    {
+        $connect = $this->createDatabaseConnect();
+
+        $this->assertSame(
+            1,
+            $connect
+                ->table('post')
+                ->insert([
+                    'title'     => 'hello world',
+                    'user_id'   => 1,
+                    'summary'   => 'post summary',
+                    'delete_at' => 0,
+                ]));
+
+        $select = new Select(new Post());
+        $post = Select::withoutPreLoadsResult(function () use ($select) {
+            return $select->findEntity(1);
+        });
+
+        $this->assertInstanceof(Post::class, $post);
+        $this->assertSame(1, $post->id);
+        $this->assertSame(1, $post->userId);
+        $this->assertSame('hello world', $post->title);
+        $this->assertSame('post summary', $post->summary);
+    }
+
+    public function testWithoutPreLoadsResultWithException(): void
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage(
+            'test exception'
+        );
+
+        Select::withoutPreLoadsResult(function () {
+            throw new Exception('test exception');
+        });
+    }
+
+    public function testPreLoadPage(): void
+    {
+        $connect = $this->createDatabaseConnect();
+
+        $this->assertSame(
+            1,
+            $connect
+                ->table('post')
+                ->insert([
+                    'title'     => 'hello world',
+                    'user_id'   => 1,
+                    'summary'   => 'post summary',
+                    'delete_at' => 0,
+                ]));
+
+        $this->assertSame(
+            2,
+            $connect
+                ->table('post')
+                ->insert([
+                    'title'     => 'hello world',
+                    'user_id'   => 1,
+                    'summary'   => 'post summary',
+                    'delete_at' => 0,
+                ]));
+
+        $select = new Select($post = Post::select()->findEntity(1));
+        $select->eager(['user']);
+        $page = $select->page(1, 10);
+        $sql = <<<'eot'
+            SQL: [52] SELECT `user`.* FROM `user` WHERE `user`.`id` IN (1) | Params:  0
+            eot;
+        $this->assertSame(
+            $sql,
+            $select->getLastSql(),
+        );
+
+        $this->assertInstanceof(Page::class, $page);
+        $data = $page->getData();
+        $this->assertCount(2, $data);
+        $this->assertInstanceof(Collection::class, $data);
+        $pageData = $page->toArray();
+        $this->assertCount(2, $pageData['data']);
+        $this->assertInstanceof(Collection::class, $pageData['data']);
+
+        $data = <<<'eot'
+            {
+                "per_page": 10,
+                "current_page": 1,
+                "total_page": 1,
+                "total_record": 2,
+                "total_macro": false,
+                "from": 0,
+                "to": 2
+            }
+            eot;
+
+        $this->assertSame(
+            $data,
+            $this->varJson(
+                $pageData['page']
+            )
         );
     }
 
