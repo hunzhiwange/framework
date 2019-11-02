bad1854dacd0b8e380dbf22c8bc5a8e6f5b3243f
XiangMin Liu <635750556@qq.com>
2019-11-02 16:34:57 +0800

    docs(database): add doc for aggregate query

--- tests/Database/Read/AggregateTest.php
+++ tests/Database/Read/AggregateTest.php
@@ -30,13 +30,25 @@ use Tests\Database\DatabaseTestCase as TestCase;
  * @since 2018.06.23
  *
  * @version 1.0
+ *
+ * @api(
+ *     zh-CN:title="聚合查询.aggregate",
+ *     path="database/read/aggregate",
+ *     description="",
+ * )
  */
 class AggregateTest extends TestCase
 {
+    /**
+     * @api(
+     *     zh-CN:title="findCount 查询数量",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
     public function testCount(): void
     {
         $connect = $this->createDatabaseConnectMock();
-
         $sql = <<<'eot'
             [
                 "SELECT COUNT(*) AS row_count FROM `test_query` LIMIT 1",
@@ -57,6 +69,28 @@ class AggregateTest extends TestCase
                     ->findCount()
             )
         );
+    }
+
+    /**
+     * @api(
+     *     zh-CN:title="count.find 查询数量",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
+    public function testCount2(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
+        $sql = <<<'eot'
+            [
+                "SELECT COUNT(*) AS row_count FROM `test_query` LIMIT 1",
+                [],
+                false,
+                null,
+                null,
+                []
+            ]
+            eot;
 
         $this->assertSame(
             $sql,
@@ -69,7 +103,18 @@ class AggregateTest extends TestCase
                 1
             )
         );
+    }
 
+    /**
+     * @api(
+     *     zh-CN:title="findCount 查询数量指定字段和别名",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
+    public function testCount3(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
         $sql = <<<'eot'
             [
                 "SELECT COUNT(*) AS row_count2 FROM `test_query` LIMIT 1",
@@ -93,10 +138,16 @@ class AggregateTest extends TestCase
         );
     }
 
+    /**
+     * @api(
+     *     zh-CN:title="findAvg 查询平均值",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
     public function testAvg(): void
     {
         $connect = $this->createDatabaseConnectMock();
-
         $sql = <<<'eot'
             [
                 "SELECT AVG(`test_query`.`num`) AS avg_value FROM `test_query` LIMIT 1",
@@ -117,6 +168,28 @@ class AggregateTest extends TestCase
                     ->findAvg('num')
             )
         );
+    }
+
+    /**
+     * @api(
+     *     zh-CN:title="avg.find 查询平均值",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
+    public function testAvg2(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
+        $sql = <<<'eot'
+            [
+                "SELECT AVG(`test_query`.`num`) AS avg_value FROM `test_query` LIMIT 1",
+                [],
+                false,
+                null,
+                null,
+                []
+            ]
+            eot;
 
         $this->assertSame(
             $sql,
@@ -129,7 +202,18 @@ class AggregateTest extends TestCase
                 1
             )
         );
+    }
 
+    /**
+     * @api(
+     *     zh-CN:title="findAvg 查询平均值指定字段和别名",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
+    public function testAvg3(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
         $sql = <<<'eot'
             [
                 "SELECT AVG(`test_query`.`num`) AS avg_value2 FROM `test_query` LIMIT 1",
@@ -153,10 +237,16 @@ class AggregateTest extends TestCase
         );
     }
 
+    /**
+     * @api(
+     *     zh-CN:title="findMax 查询最大值",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
     public function testMax(): void
     {
         $connect = $this->createDatabaseConnectMock();
-
         $sql = <<<'eot'
             [
                 "SELECT MAX(`test_query`.`num`) AS max_value FROM `test_query` LIMIT 1",
@@ -177,6 +267,28 @@ class AggregateTest extends TestCase
                     ->findMax('num')
             )
         );
+    }
+
+    /**
+     * @api(
+     *     zh-CN:title="max.find 查询最大值",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
+    public function testMax2(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
+        $sql = <<<'eot'
+            [
+                "SELECT MAX(`test_query`.`num`) AS max_value FROM `test_query` LIMIT 1",
+                [],
+                false,
+                null,
+                null,
+                []
+            ]
+            eot;
 
         $this->assertSame(
             $sql,
@@ -189,7 +301,18 @@ class AggregateTest extends TestCase
                 1
             )
         );
+    }
 
+    /**
+     * @api(
+     *     zh-CN:title="findMax 查询最大值指定字段和别名",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
+    public function testMax3(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
         $sql = <<<'eot'
             [
                 "SELECT MAX(`test_query`.`num`) AS max_value2 FROM `test_query` LIMIT 1",
@@ -213,10 +336,16 @@ class AggregateTest extends TestCase
         );
     }
 
+    /**
+     * @api(
+     *     zh-CN:title="findMin 查询最小值",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
     public function testMin(): void
     {
         $connect = $this->createDatabaseConnectMock();
-
         $sql = <<<'eot'
             [
                 "SELECT MIN(`test_query`.`num`) AS min_value FROM `test_query` LIMIT 1",
@@ -237,6 +366,28 @@ class AggregateTest extends TestCase
                     ->findMin('num')
             )
         );
+    }
+
+    /**
+     * @api(
+     *     zh-CN:title="min.find 查询最小值",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
+    public function testMin2(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
+        $sql = <<<'eot'
+            [
+                "SELECT MIN(`test_query`.`num`) AS min_value FROM `test_query` LIMIT 1",
+                [],
+                false,
+                null,
+                null,
+                []
+            ]
+            eot;
 
         $this->assertSame(
             $sql,
@@ -249,7 +400,18 @@ class AggregateTest extends TestCase
                 1
             )
         );
+    }
 
+    /**
+     * @api(
+     *     zh-CN:title="findMin 查询最小值指定字段和别名",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
+    public function testMin3(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
         $sql = <<<'eot'
             [
                 "SELECT MIN(`test_query`.`num`) AS min_value2 FROM `test_query` LIMIT 1",
@@ -273,10 +435,16 @@ class AggregateTest extends TestCase
         );
     }
 
+    /**
+     * @api(
+     *     zh-CN:title="findSum 查询合计",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
     public function testSum(): void
     {
         $connect = $this->createDatabaseConnectMock();
-
         $sql = <<<'eot'
             [
                 "SELECT SUM(`test_query`.`num`) AS sum_value FROM `test_query` LIMIT 1",
@@ -297,6 +465,28 @@ class AggregateTest extends TestCase
                     ->findSum('num')
             )
         );
+    }
+
+    /**
+     * @api(
+     *     zh-CN:title="sum.find 查询合计",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
+    public function testSum2(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
+        $sql = <<<'eot'
+            [
+                "SELECT SUM(`test_query`.`num`) AS sum_value FROM `test_query` LIMIT 1",
+                [],
+                false,
+                null,
+                null,
+                []
+            ]
+            eot;
 
         $this->assertSame(
             $sql,
@@ -309,7 +499,18 @@ class AggregateTest extends TestCase
                 1
             )
         );
+    }
 
+    /**
+     * @api(
+     *     zh-CN:title="findSum 查询合计指定字段和别名",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
+    public function testSum3(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
         $sql = <<<'eot'
             [
                 "SELECT SUM(`test_query`.`num`) AS sum_value2 FROM `test_query` LIMIT 1",
@@ -333,10 +534,16 @@ class AggregateTest extends TestCase
         );
     }
 
+    /**
+     * @api(
+     *     zh-CN:title="findAvg 查询字段指定表名",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
     public function testAvgWithTable(): void
     {
         $connect = $this->createDatabaseConnectMock();
-
         $sql = <<<'eot'
             [
                 "SELECT AVG(`test_query`.`num`) AS avg_value FROM `test_query` LIMIT 1",
@@ -357,6 +564,28 @@ class AggregateTest extends TestCase
                     ->findAvg('test_query.num')
             )
         );
+    }
+
+    /**
+     * @api(
+     *     zh-CN:title="avg.find 查询字段指定表名",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
+    public function testAvgWithTable2(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
+        $sql = <<<'eot'
+            [
+                "SELECT AVG(`test_query`.`num`) AS avg_value FROM `test_query` LIMIT 1",
+                [],
+                false,
+                null,
+                null,
+                []
+            ]
+            eot;
 
         $this->assertSame(
             $sql,
