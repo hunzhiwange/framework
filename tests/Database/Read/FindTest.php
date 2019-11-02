4c583e46438009e9d1bd701a380fad216433a741
XiangMin Liu <635750556@qq.com>
2019-11-02 17:53:14 +0800

    docs(database): add doc for find

--- tests/Database/Read/FindTest.php
+++ tests/Database/Read/FindTest.php
@@ -30,9 +30,22 @@ use Tests\Database\DatabaseTestCase as TestCase;
  * @since 2018.06.21
  *
  * @version 1.0
+ *
+ * @api(
+ *     zh-CN:title="查询数据.find",
+ *     path="database/read/find",
+ *     description="",
+ * )
  */
 class FindTest extends TestCase
 {
+    /**
+     * @api(
+     *     zh-CN:title="find 查询基础用法",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
     public function testBaseUse(): void
     {
         $connect = $this->createDatabaseConnectMock();
@@ -57,6 +70,18 @@ class FindTest extends TestCase
                     ->find()
             )
         );
+    }
+
+    /**
+     * @api(
+     *     zh-CN:title="find 查询指定数量",
+     *     zh-CN:description="",
+     *     note="",
+     * )
+     */
+    public function testFindLimit(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
 
         $sql = <<<'eot'
             [
