7a38fd44baf928d9c25b0531aa8acfd2f7e2e82b
XiangMin Liu <635750556@qq.com>
2019-11-11 22:51:56 +0800

    style(code): remove empty line

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -28,15 +28,11 @@ declare(strict_types=1);
  * @version 1.0
  */
 error_reporting(-1);
-
 ini_set('xdebug.max_nesting_level', '200');
-
 ini_set('memory_limit', '512M');
 
 $vendorDir = __DIR__.'/../vendor';
-
 require_once __DIR__.'/function.php';
-
 require_once __DIR__.'/config.php';
 
 if (false === is_file($vendorDir.'/autoload.php')) {
