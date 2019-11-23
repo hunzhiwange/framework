8ed0baa8cdc90321e239b6478d517bc324db3f38
XiangMin Liu <635750556@qq.com>
2019-11-23 15:46:04 +0800

    docs(support): add doc for \Leevel\Support\Type

--- tests/Support/TypeTest.php
+++ tests/Support/TypeTest.php
@@ -32,9 +32,22 @@ use Tests\TestCase;
  * @since 2018.06.10
  *
  * @version 1.0
+ *
+ * @api(
+ *     title="类型",
+ *     path="component/support/type",
+ *     description="QueryPHP 提供了增加 PHP 自身类型的辅助方法。",
+ * )
  */
 class TypeTest extends TestCase
 {
+    /**
+     * @api(
+     *     title="判断是否为字符串",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeString(): void
     {
         // string
@@ -43,6 +56,13 @@ class TypeTest extends TestCase
         $this->assertFalse(Type::type(1, 'str'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为整型",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeInt(): void
     {
         // int
@@ -51,6 +71,13 @@ class TypeTest extends TestCase
         $this->assertFalse(Type::type(true, 'int'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为浮点数",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeFloat(): void
     {
         // float
@@ -59,6 +86,13 @@ class TypeTest extends TestCase
         $this->assertFalse(Type::type(true, 'double'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为布尔值",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeBool(): void
     {
         // bool
@@ -67,6 +101,13 @@ class TypeTest extends TestCase
         $this->assertFalse(Type::type(4, 'boolean'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为数字",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeNum(): void
     {
         // num
@@ -76,6 +117,13 @@ class TypeTest extends TestCase
         $this->assertFalse(Type::type(false, 'numeric'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为标量",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeBase(): void
     {
         // base
@@ -89,6 +137,13 @@ class TypeTest extends TestCase
         $this->assertFalse(Type::type(null, 'scalar'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为资源",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeResource(): void
     {
         // resource
@@ -101,6 +156,13 @@ class TypeTest extends TestCase
         unlink($testFile);
     }
 
+    /**
+     * @api(
+     *     title="判断是否为闭包",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeClosure(): void
     {
         // closure
@@ -109,6 +171,13 @@ class TypeTest extends TestCase
         $this->assertFalse(Type::type(true, 'closure'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为数组",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeArray(): void
     {
         // array
@@ -119,6 +188,13 @@ class TypeTest extends TestCase
         $this->assertTrue(Type::type([1, 2], 'arr:int'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为对象",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeObject(): void
     {
         // object
@@ -127,6 +203,13 @@ class TypeTest extends TestCase
         $this->assertFalse(Type::type(null, 'object'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为 NULL",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeNull(): void
     {
         // null
@@ -134,6 +217,19 @@ class TypeTest extends TestCase
         $this->assertFalse(Type::type(1, 'null'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为回调",
+     *     description="
+     * **\Tests\Support\Callback1 定义**
+     *
+     * ``` php
+     * {[\Leevel\Kernel\Utils\Doc::getClassBody(\Tests\Support\Callback1::class)]}
+     * ```
+     * ",
+     *     note="",
+     * )
+     */
     public function testTypeCallback(): void
     {
         // callback
@@ -145,6 +241,25 @@ class TypeTest extends TestCase
         $this->assertFalse(Type::type(1, 'callback'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为对象实例",
+     *     description="
+     * **\Tests\Support\Callback2 定义**
+     *
+     * ``` php
+     * {[\Leevel\Kernel\Utils\Doc::getClassBody(\Tests\Support\Callback2::class)]}
+     * ```
+     *
+     * **\Tests\Support\IInterface 定义**
+     *
+     * ``` php
+     * {[\Leevel\Kernel\Utils\Doc::getClassBody(\Tests\Support\IInterface::class)]}
+     * ```
+     * ",
+     *     note="",
+     * )
+     */
     public function testTypeInstance(): void
     {
         // instance
@@ -154,6 +269,13 @@ class TypeTest extends TestCase
         $this->assertFalse(Type::type(1, 'callback'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为数字或者字符串数字",
+     *     description="包括短横线、英文逗号组成，比如日期、千分位等。",
+     *     note="",
+     * )
+     */
     public function testTypeNumeric(): void
     {
         $this->assertTrue(Type::typeNumeric(2.2));
@@ -164,12 +286,26 @@ class TypeTest extends TestCase
         $this->assertTrue(Type::typeNumeric('2,111,500-200'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为整型或者字符串整型",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeStringInt(): void
     {
         $this->assertTrue(Type::typeInt(1));
         $this->assertTrue(Type::typeInt('4'));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为指定的几种类型",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeThese(): void
     {
         $this->assertTrue(Type::typeThese('foo', ['string']));
@@ -180,10 +316,18 @@ class TypeTest extends TestCase
     public function testTypeTheseException(): void
     {
         $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The param must be string or an array of string elements.');
 
         $this->assertTrue(Type::typeThese('foo', [[]]));
     }
 
+    /**
+     * @api(
+     *     title="判断是否为数组元素类型",
+     *     description="",
+     *     note="",
+     * )
+     */
     public function testTypeStrictArray(): void
     {
         $this->assertFalse(Type::typeArray('foo', ['string']));
