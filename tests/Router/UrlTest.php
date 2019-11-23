1cad623fc3cdf28a8c4fed1f284a883548684c4d
XiangMin Liu <635750556@qq.com>
2019-11-23 16:14:31 +0800

    tests(router): split tests of url

--- tests/Router/UrlTest.php
+++ tests/Router/UrlTest.php
@@ -32,6 +32,25 @@ use Tests\TestCase;
  * @since 2018.02.06
  *
  * @version 1.0
+ *
+ * @api(
+ *     title="URL 生成",
+ *     path="router/url",
+ *     description="QueryPHP 支持路由 URL 地址的统一生成，提供一套简洁的生成方法，无需记忆即可学会使用。
+ *
+ * 使用助手函数
+ *
+ * ``` php
+ * \Leevel\Router\Helper::url(string $url, array $params = [], string $subdomain = 'www', $suffix = null): string;
+ * ```
+ *
+ * 使用容器 url 服务
+ *
+ * ``` php
+ * \App::make('url')->make(string $url, array $params = [], string $subdomain = 'www', $suffix = null): string;
+ * ```
+ * ",
+ * )
  */
 class UrlTest extends TestCase
 {
@@ -39,22 +58,63 @@ class UrlTest extends TestCase
     {
         $request = $this->makeRequest();
         $url = new Url($request);
-
         $this->assertInstanceof(IRequest::class, $url->getRequest());
 
         // 开始不带斜线，自动添加
-        $this->assertSame($url->make('/'), '/');
-        $this->assertSame($url->make(''), '/');
         $this->assertSame($url->make('test/hello'), '/test/hello');
+    }
+
+    public function testMakeUrlWithParams(): void
+    {
+        $request = $this->makeRequest();
+        $url = new Url($request);
+
         $this->assertSame($url->make('test/hello?arg1=1&arg2=3'), '/test/hello?arg1=1&arg2=3');
         $this->assertSame($url->make('test/sub1/sub2/hello?arg1=1&arg2=3'), '/test/sub1/sub2/hello?arg1=1&arg2=3');
-        $this->assertSame($url->make(':myapp/hello/world', ['id' => 5, 'name' => 'yes']), '/:myapp/hello/world?id=5&name=yes');
-        $this->assertSame($url->make(':myapp/test'), '/:myapp/test');
+        $this->assertSame($url->make('test/sub1/sub2/hello', ['arg1' => 1, 'arg2' => 3]), '/test/sub1/sub2/hello?arg1=1&arg2=3');
+    }
+
+    public function testMakeUrlWithSuffix(): void
+    {
+        $request = $this->makeRequest();
+        $url = new Url($request);
+
         $this->assertSame($url->make('hello/world', [], '', true), '/hello/world.html');
         $this->assertSame($url->make('hello/world', [], '', '.jsp'), '/hello/world.jsp');
+    }
+
+    public function testMakeUrlForApp(): void
+    {
+        $request = $this->makeRequest();
+        $url = new Url($request);
+
+        $this->assertSame($url->make(':myapp/hello/world', ['id' => 5, 'name' => 'yes']), '/:myapp/hello/world?id=5&name=yes');
+        $this->assertSame($url->make(':myapp/test'), '/:myapp/test');
+    }
+
+    public function testMakeUrlForHome(): void
+    {
+        $request = $this->makeRequest();
+        $url = new Url($request);
+
+        $this->assertSame($url->make('/'), '/');
+        $this->assertSame($url->make(''), '/');
+    }
+
+    public function testMakeUrlForAbsolute(): void
+    {
+        $request = $this->makeRequest();
+        $url = new Url($request);
 
         // 开始可带斜线
         $this->assertSame($url->make('/hello-world'), '/hello-world');
+    }
+
+    public function testMakeUrlForAbsoluteSupportVar(): void
+    {
+        $request = $this->makeRequest();
+        $url = new Url($request);
+
         $this->assertSame($url->make('/new-{id}-{name}', ['id' => 5, 'name' => 'tom', 'arg1' => '5']), '/new-5-tom?arg1=5');
         $this->assertSame($url->make('/new-{id}-{name}?hello=world', ['id' => 5, 'name' => 'tom', 'arg1' => '5']), '/new-5-tom?hello=world&arg1=5');
         $this->assertSame($url->make('/new-{id}-{name}?hello={foo}', ['id' => 5, 'name' => 'tom', 'foo' => 'bar', 'arg1' => '5']), '/new-5-tom?hello=bar&arg1=5');
@@ -90,7 +150,6 @@ class UrlTest extends TestCase
         $url = new Url($request);
 
         $this->assertInstanceof(IRequest::class, $url->getRequest());
-
         $this->assertSame($url->make('hello/world'), '/hello/world');
     }
 
@@ -100,7 +159,6 @@ class UrlTest extends TestCase
         $url = new Url($request);
 
         $this->assertInstanceof(IRequest::class, $url->getRequest());
-
         $this->assertSame('/hello/{foo}', $url->make('hello/{foo}', []));
     }
 
@@ -112,7 +170,6 @@ class UrlTest extends TestCase
         ]);
 
         $this->assertInstanceof(IRequest::class, $url->getRequest());
-
         $this->assertSame($url->make('hello/world'), 'https://www.queryphp.cn/hello/world');
     }
 
