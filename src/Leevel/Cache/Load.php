44c7456d2a64cb9fb138e69a7dccb1d164dbda41
XiangMin Liu <635750556@qq.com>
2019-11-29 12:42:07 +0800

    style(cache): code poem

--- src/Leevel/Cache/Load.php
+++ src/Leevel/Cache/Load.php
@@ -60,8 +60,9 @@ class Load implements ILoad
     }
 
     /**
-     * 载入缓存数据
-     * 系统自动存储缓存到内存，可重复执行不会重复载入数据.
+     * 载入缓存数据.
+     *
+     * - 系统自动存储缓存到内存，可重复执行不会重复载入数据.
      *
      * @param array $names
      * @param array $option
