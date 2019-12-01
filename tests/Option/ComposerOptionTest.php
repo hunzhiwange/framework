ac350b476a111e7d4a776faba52aab02605ddc18
XiangMin Liu <635750556@qq.com>
2019-12-01 21:54:38 +0800

    docs(option): update option doc

--- tests/Option/ComposerOptionTest.php
+++ tests/Option/ComposerOptionTest.php
@@ -31,9 +31,30 @@ use Tests\TestCase;
  * @since 2018.07.17
  *
  * @version 1.0
+ *
+ * @api(
+ *     title="Composer 扩展配置",
+ *     path="component/option/composer",
+ *     description="QueryPHP 系统服务提供者、应用命令、扩展配置和扩展语言包等都在 `composer` 中进行定义。",
+ * )
  */
 class ComposerOptionTest extends TestCase
 {
+    /**
+     * @api(
+     *     title="composer.json ",
+     *     description="
+     * 示例配置
+     *
+     * ``` json
+     * {[file_get_contents('vendor/hunzhiwange/framework/tests/Option/app1/composer.json')]}
+     * ```
+     *
+     * 相关配置约定在 `leevel` 字段中，可以非常方便地扩展系统。
+     * ",
+     *     note="",
+     * )
+     */
     public function testBaseUse(): void
     {
         $options = ($composerOption = new ComposerOption(__DIR__.'/app1'))->loadData();
