fd83520e0b63ef8a8388e9e5d5bcad5b7222c81a
XiangMin Liu <635750556@qq.com>
2019-12-03 12:39:59 +0800

    docs(cache): add doc for cache

--- tests/Cache/CacheTest.php
+++ tests/Cache/CacheTest.php
@@ -32,6 +32,72 @@ use Tests\TestCase;
  * @since 2018.07.29
  *
  * @version 1.0
+ *
+ * @api(
+ *     title="缓存",
+ *     path="component/cache",
+ *     description="
+ * QueryPHP 为系统提供了灵活的缓存功能，提供了多种缓存驱动。
+ *
+ * 内置支持的缓存类型包括 file、redis，未来可能增加其他驱动。
+ *
+ * ## 使用方式
+ *
+ * 使用助手函数
+ *
+ * ``` php
+ * \Leevel\Cache\Helper::cache_get(string $key, $defaults = null, array $option = []);
+ * \Leevel\Cache\Helper::cache_get(string $key, $defaults = null, array $option = []);
+ * \Leevel\Cache\Helper::function cache(): \Leevel\Cache\ICache;
+ * ```
+ *
+ * 使用容器 cache 服务
+ *
+ * ``` php
+ * \App::make('cache')->set(string $name, $data, array $option = []): void;
+ * \App::make('cache')->get(string $name, $defaults = false, array $option = []);
+ * ```
+ *
+ * 依赖注入
+ *
+ * ``` php
+ * class Demo
+ * {
+ *     private $cache;
+ *
+ *     public function __construct(\Leevel\Cache\ICache $cache)
+ *     {
+ *         $this->cache = $cache;
+ *     }
+ * }
+ * ```
+ *
+ * 使用静态代理
+ *
+ * ``` php
+ * \Leevel\Cache\Proxy\Cache::set(string $name, $data, array $option = []): void;
+ * \Leevel\Cache\Proxy\Cache::get(string $name, $defaults = false, array $option = []);
+ * ```
+ *
+ * ## 缓存配置
+ *
+ * 系统的缓存配置位于应用下面的 `option/cache.php` 文件。
+ *
+ * 可以定义多个缓存连接，并且支持切换，每一个连接支持驱动设置。
+ *
+ * ``` php
+ * {[file_get_contents('option/cache.php')]}
+ * ```
+ *
+ * 缓存参数根据不同的连接会有所区别，通用的缓存参数如下：
+ *
+ * |配置项|配置描述|
+ * |:-|:-|
+ * |expire|设置好缓存时间（小与等于 0 表示永不过期，单位时间为秒）|
+ * |time_preset|缓存时间预置|
+ * |serialize|是否使用 serialize 编码|
+ * ",
+ * )
  */
 class CacheTest extends TestCase
 {
