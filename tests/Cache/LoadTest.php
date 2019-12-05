8763de2dd09b3029dd86061fa91417c20e1fc2ce
XiangMin Liu <635750556@qq.com>
2019-12-05 20:58:30 +0800

    docs(cache): add doc for cache load

--- tests/Cache/LoadTest.php
+++ tests/Cache/LoadTest.php
@@ -35,6 +35,41 @@ use Tests\TestCase;
  * @since 2018.07.29
  *
  * @version 1.0
+ *
+ * @api(
+ *     title="缓存块载入",
+ *     path="component/cache/load",
+ *     description="
+ * QueryPHP 提供了缓存块自动载入功能，缓存块类似于缓存的 `remember` 功能，一个类代表一个缓存块。
+ *
+ * ### 定义缓存块
+ *
+ * **例子 \Tests\Cache\Pieces\Test1**
+ *
+ * ``` php
+ * {[\Leevel\Kernel\Utils\Doc::getClassBody(\Tests\Cache\Pieces\Test1::class)]}
+ * ```
+ *
+ * 缓冲块需要实现 `\Leevel\Cache\IBlock` 接口，即可统一进行管理。
+ *
+ * **接口 \Leevel\Cache\IBlock**
+ *
+ * ``` php
+ * {[\Leevel\Kernel\Utils\Doc::getClassBody(\Leevel\Cache\IBlock::class)]}
+ * ```
+ *
+ * 缓存块实现非常灵活，可以非常轻松地使用。
+ *
+ * ### 载入缓存片段
+ *
+ * 缓存载入服务 `cache.load` 被系统注册到服务容器中了，可以使用代理 `proxy` 来调用。
+ *
+ * ``` php
+ * \Leevel\Cache\Proxy\Load::data(array $names, array $option = [], bool $force = false): array;
+ * \Leevel\Cache\Proxy\Load::refresh(array $names): void;
+ * ```
+ * ",
+ * )
  */
 class LoadTest extends TestCase
 {
@@ -53,12 +88,28 @@ class LoadTest extends TestCase
         }
 
         $path = __DIR__.'/Pieces/cacheLoad';
-
         if (is_dir($path)) {
             rmdir($path);
         }
     }
 
+    /**
+     * @api(
+     *     title="data 载入缓存块数据",
+     *     description="
+     * 通过 `data` 即可载入缓存块数据，缓存直接传递缓存块的类名字即可。
+     *
+     * ``` php
+     * data(array $names, array $option = [], bool $force = false): array;
+     * ```
+     *
+     * ::: tip
+     * 配置 `$option` 和 缓存功能中的 `set` 的用法一致。
+     * :::
+     * ",
+     *     note="",
+     * )
+     */
     public function testBaseUse(): void
     {
         $container = new Container();
@@ -73,6 +124,23 @@ class LoadTest extends TestCase
         $load->refresh([Test1::class]);
     }
 
+    /**
+     * @api(
+     *     title="refresh 刷新缓存块数据",
+     *     description="
+     * 通过 `refresh` 即可刷新缓存块数据，缓存直接传递缓存块的类名字即可。
+     *
+     * ``` php
+     * refresh(array $names): void;
+     * ```
+     *
+     * ::: tip
+     * 刷新缓存块本质是删除缓存块数据，下次请求自动生成。
+     * :::
+     * ",
+     *     note="",
+     * )
+     */
     public function testRefresh(): void
     {
         $container = new Container();
@@ -91,6 +159,13 @@ class LoadTest extends TestCase
         $this->assertFalse(is_file($file));
     }
 
+    /**
+     * @api(
+     *     title="data 强制载入缓存块数据",
+     *     description="`data` 方法支持强制获取缓存数据。",
+     *     note="`$force` 参数强制从原始数据源获取缓存，并且会刷新缓存数据。",
+     * )
+     */
     public function testDataForce(): void
     {
         $container = new Container();
@@ -116,6 +191,25 @@ class LoadTest extends TestCase
         $load->data([Test2::class]);
     }
 
+    /**
+     * @api(
+     *     title="data 载入缓存块数据支持参数",
+     *     description="
+     * `data` 方法支持传递一些参数到缓存块，可以生成不同的缓存数据。
+     *
+     * **例子 \Tests\Cache\Pieces\Test1**
+     *
+     * ``` php
+     * {[\Leevel\Kernel\Utils\Doc::getClassBody(\Tests\Cache\Pieces\Test4::class)]}
+     * ```
+     *
+     * ::: tip
+     * 参数通过 `:` 冒号进行分割，冒号后边是自定义参数。
+     * :::
+     * ",
+     *     note="",
+     * )
+     */
     public function testWithParams(): void
     {
         $container = new Container();
@@ -134,7 +228,6 @@ class LoadTest extends TestCase
 
         $container = new Container();
         $load = $this->createLoad($container);
-
         $load->data(['Tests\Cache\Pieces\TestNotFound']);
     }
 
