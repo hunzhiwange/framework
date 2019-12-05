a02e0dc71d67363832c7ca1ca65b6fb347edf37c
XiangMin Liu <635750556@qq.com>
2019-12-05 21:36:24 +0800

    style(router): code poem

--- src/Leevel/Router/IResponse.php
+++ src/Leevel/Router/IResponse.php
@@ -100,98 +100,111 @@ interface IResponse
     public function redirectRaw(string $url, int $status = 302, array $headers = []): RedirectResponse;
 
     /**
-     * 请求成功
-     * 一般用于GET与POST请求: 200.
+     * 请求成功.
+     *
+     * - 一般用于GET与POST请求: 200.
      *
      * @param mixed $content
      */
     public function apiOk($content = '', ?string $text = null): ApiResponse;
 
     /**
-     * 已创建
-     * 成功请求并创建了新的资源: 201.
+     * 已创建.
+     *
+     * - 成功请求并创建了新的资源: 201.
      *
      * @param mixed $content
      */
     public function apiCreated(?string $location = '', $content = ''): ApiResponse;
 
     /**
-     * 已接受
-     * 已经接受请求，但未处理完成: 202.
+     * 已接受.
+     *
+     * - 已经接受请求，但未处理完成: 202.
      *
      * @param mixed $content
      */
     public function apiAccepted(?string $location = null, $content = ''): ApiResponse;
 
     /**
-     * 无内容
-     * 服务器成功处理，但未返回内容: 204.
+     * 无内容.
+     *
+     * - 服务器成功处理，但未返回内容: 204.
      */
     public function apiNoContent(): ApiResponse;
 
     /**
-     * 错误请求
-     * 服务器不理解请求的语法: 400.
+     * 错误请求.
+     *
+     * - 服务器不理解请求的语法: 400.
      */
     public function apiError(string $message, int $statusCode, ?string $text = null): ApiResponse;
 
     /**
-     * 错误请求
-     * 服务器不理解请求的语法: 400.
+     * 错误请求.
+     *
+     * - 服务器不理解请求的语法: 400.
      */
     public function apiBadRequest(?string $message = null, ?string $text = null): ApiResponse;
 
     /**
-     * 未授权
-     * 对于需要登录的网页，服务器可能返回此响应: 401.
+     * 未授权.
+     *
+     * - 对于需要登录的网页，服务器可能返回此响应: 401.
      */
     public function apiUnauthorized(?string $message = null, ?string $text = null): ApiResponse;
 
     /**
-     * 禁止
-     * 服务器拒绝请求: 403.
+     * 禁止.
+     *
+     * - 服务器拒绝请求: 403.
      */
     public function apiForbidden(?string $message = null, ?string $text = null): ApiResponse;
 
     /**
-     * 未找到
-     * 用户发出的请求针对的是不存在的记录: 404.
+     * 未找到.
+     *
+     * - 用户发出的请求针对的是不存在的记录: 404.
      */
     public function apiNotFound(?string $message = null, ?string $text = null): ApiResponse;
 
     /**
-     * 方法禁用
-     * 禁用请求中指定的方法: 405.
+     * 方法禁用.
+     *
+     * - 禁用请求中指定的方法: 405.
      */
     public function apiMethodNotAllowed(?string $message = null, ?string $text = null): ApiResponse;
 
     /**
-     * 无法处理的实体
-     * 请求格式正确，但是由于含有语义错误，无法响应: 422.
+     * 无法处理的实体.
+     *
+     * - 请求格式正确，但是由于含有语义错误，无法响应: 422.
      */
     public function apiUnprocessableEntity(?array $errors = null, ?string $message = null, ?string $text = null): ApiResponse;
 
     /**
-     * 太多请求
-     * 用户在给定的时间内发送了太多的请求: 429.
+     * 太多请求.
+     *
+     * - 用户在给定的时间内发送了太多的请求: 429.
      */
     public function apiTooManyRequests(?string $message = null, ?string $text = null): ApiResponse;
 
     /**
-     * 服务器内部错误
-     * 服务器遇到错误，无法完成请求: 500.
+     * 服务器内部错误.
+     *
+     * - 服务器遇到错误，无法完成请求: 500.
      */
     public function apiInternalServerError(?string $message = null, ?string $text = null): ApiResponse;
 
     /**
-     * 设置视图正确模板
+     * 设置视图正确模板.
      *
      * @return \Leevel\Router\IResponse
      */
     public function setViewSuccessTemplate(string $template): self;
 
     /**
-     * 设置视图错误模板
+     * 设置视图错误模板.
      *
      * @return \Leevel\Router\IResponse
      */
