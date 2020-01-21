36001fce65de49dcfef0619354e9c70214e063bd
XiangMin Liu <635750556@qq.com>
2020-01-21 20:28:14 +0800

    refactor(support): add ide helper for str helper

--- src/Leevel/Support/Str.php
+++ src/Leevel/Support/Str.php
@@ -25,6 +25,23 @@ use Leevel\Support\Str\un_camelize;
 
 /**
  * 字符串.
+ *
+ * @method static string camelize(string $value, string $separator = '_')                              下划线转驼峰.
+ * @method static bool contains(string $toSearched, string $search)                                    判断字符串中是否包含给定的字符串集合.
+ * @method static bool endsWith(string $toSearched, string $search)                                    判断字符串中是否包含给定的字符结尾.
+ * @method static string formatBytes(int $fileSize, bool $withUnit = true)                             文件大小格式化.
+ * @method static string formatDate(int $dateTemp, array $lang = [], string $dateFormat = 'Y-m-d H:i') 日期格式化.
+ * @method static string randAlpha(int $length, ?string $charBox = null)                               随机字母.
+ * @method static string randAlphaLowercase(int $length, ?string $charBox = null)                      随机小写字母.
+ * @method static string randAlphaNum(int $length, ?string $charBox = null)                            随机字母数字.
+ * @method static string randAlphaNumLowercase(int $length, ?string $charBox = null)                   随机小写字母数字.
+ * @method static string randAlphaNumUppercase(int $length, ?string $charBox = null)                   随机大写字母数字.
+ * @method static string randAlphaUppercase(int $length, ?string $charBox = null)                      随机大写字母.
+ * @method static string randChinese(int $length, ?string $charBox = null)                             随机字中文.
+ * @method static string randNum(int $length, ?string $charBox = null)                                 随机数字.
+ * @method static string randStr(int $length, string $charBox)                                         随机字符串.
+ * @method static bool startsWith(string $toSearched, string $search)                                  判断字符串中是否包含给定的字符开始.
+ * @method static string unCamelize(string $value, string $separator = '_')                            驼峰转下划线.
  */
 class Str
 {
