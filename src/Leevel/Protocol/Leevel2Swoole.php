a8a531652255c2384375ee9f5b5fca0fae1df751
XiangMin Liu <635750556@qq.com>
2020-01-26 22:44:06 +0800

    fix(protocol): fix Leevel\Protocol\Leevel2Swoole

--- src/Leevel/Protocol/Leevel2Swoole.php
+++ src/Leevel/Protocol/Leevel2Swoole.php
@@ -21,6 +21,7 @@ declare(strict_types=1);
 namespace Leevel\Protocol;
 
 use Swoole\Http\Response as SwooleHttpResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -35,21 +36,35 @@ class Leevel2Swoole
      */
     public function createResponse(Response $response, SwooleHttpResponse $swooleResponse): SwooleHttpResponse
     {
-        foreach ($response->getCookies() as $item) {
-            $swooleResponse->cookie(...$item);
-        }
-
-        if ($response instanceof RedirectResponse &&
-            method_exists($swooleResponse, 'redirect')) {
-            $swooleResponse->redirect($response->getTargetUrl());
-        }
-
-        foreach ($response->headers->all() as $key => $value) {
-            $swooleResponse->header($key, $value);
-        }
+        $this->convertRedirectTargetUrl($response, $swooleResponse);
+        $this->convertHeaders($response, $swooleResponse);
         $swooleResponse->status($response->getStatusCode());
         $swooleResponse->write($response->getContent() ?: ' ');
 
         return $swooleResponse;
+    }
+
+    /**
+     * 转换跳转地址.
+     */
+    protected function convertRedirectTargetUrl(Response $response, SwooleHttpResponse $swooleResponse): void
+    {
+        if ($response instanceof RedirectResponse &&
+            method_exists($swooleResponse, 'redirect')) {
+            $swooleResponse->redirect($response->getTargetUrl());
+        }
+    }
+
+    /**
+     * 转换响应头.
+     */
+    protected function convertHeaders(Response $response, SwooleHttpResponse $swooleResponse): void
+    {
+        foreach ($response->headers->all() as $name => $values) {
+            $name = ucwords($name, '-');
+            foreach ($values as $value) {
+                $swooleResponse->header($name, $value);
+            }
+        }
     }
 }
