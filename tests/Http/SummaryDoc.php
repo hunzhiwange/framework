9b5a44e95d4d28d3fd6046e443804413932e4437
XiangMin Liu <635750556@qq.com>
2020-02-10 09:57:29 +0800

    docs(http): fix tests/Http/SummaryDoc.php

--- tests/Http/SummaryDoc.php
+++ tests/Http/SummaryDoc.php
@@ -29,7 +29,7 @@ namespace Tests\Http;
  *     description="
  * QueryPHP 基于 Symfony 请求组件进行了包装，增加了少量功能。
  *
- * HTTP 基本使用方法可以查看 [Symfony Request](https://symfony.com/doc/current/components/http_foundation.html)。
+ * HTTP 基本使用方法可以查看 [Symfony HttpFoundation](https://symfony.com/doc/current/components/http_foundation.html)。
  * ",
  * )
  */
