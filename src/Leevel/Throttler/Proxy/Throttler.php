a500d50612b4b35c35fdd341d610192bf9edae06
XiangMin Liu <635750556@qq.com>
2020-02-12 11:34:18 +0800

    fix(throttler): fix return type of Leevel\ThrottlerProxy\Throttler create

--- src/Leevel/Throttler/Proxy/Throttler.php
+++ src/Leevel/Throttler/Proxy/Throttler.php
@@ -22,8 +22,8 @@ namespace Leevel\Throttler\Proxy;
 
 use Leevel\Di\Container;
 use Leevel\Http\Request;
-use Leevel\Throttler\IRateLimiter;
 use Leevel\Throttler\IThrottler as IBaseThrottler;
+use Leevel\Throttler\RateLimiter;
 use Leevel\Throttler\Throttler as BaseThrottler;
 
 /**
@@ -46,7 +46,7 @@ class Throttler
     /**
      * 创建一个节流器.
      */
-    public static function create(?string $key = null, int $xRateLimitLimit = 20, int $xRateLimitTime = 20): IRateLimiter
+    public static function create(?string $key = null, int $xRateLimitLimit = 20, int $xRateLimitTime = 20): RateLimiter
     {
         return self::proxy()->create($key, $xRateLimitLimit, $xRateLimitTime);
     }
