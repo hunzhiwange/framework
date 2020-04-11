7a04773ed44534a20174138e753e53f0c7284d64
XiangMin Liu <635750556@qq.com>
2020-04-11 19:22:32 +0800

    fix(support): fix for phpstan level 8

--- src/Leevel/Support/functions.php
+++ src/Leevel/Support/functions.php
@@ -52,7 +52,7 @@ if (!function_exists('f_exists')) {
             return true;
         }
 
-        $virtualClass = null;
+        $virtualClass = '';
         foreach (['fn', 'prefix', 'index'] as $type) {
             switch ($type) {
                 case 'fn':
