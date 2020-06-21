392f4cb7859b54ffbf127d8adf049213099657bd
XiangMin Liu <635750556@qq.com>
2020-06-21 13:53:51 +0800

    refactor(tests): test config

--- tests/config.php
+++ tests/config.php
@@ -18,13 +18,11 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
- // 配置.
+if (file_exists(__DIR__.'/config.local.php')) {
+    require __DIR__.'/config.local.php';
 
- if (file_exists(__DIR__.'/config.local.php')) {
-     require __DIR__.'/config.local.php';
-
-     return;
- }
+    return;
+}
 
 $GLOBALS['LEEVEL_ENV']['DATABASE']['MYSQL'] = [
     'HOST'     => '127.0.0.1',
