364f5928fcc02cd6b19c04cfc767776990340ff9
XiangMin Liu <635750556@qq.com>
2020-07-17 23:43:32 +0800

    refactor(kernel): add DEFAULT_LEVEL for BusinessException

--- src/Leevel/Kernel/Exception/BusinessException.php
+++ src/Leevel/Kernel/Exception/BusinessException.php
@@ -30,6 +30,13 @@ use RuntimeException;
 abstract class BusinessException extends RuntimeException
 {
     /**
+     * 默认 0 表示不是很重要的业务日志.
+     *
+     * @var int
+     */
+    const DEFAULT_LEVEL = 0;
+
+    /**
      * 业务逻辑异常重要程度.
      *
      * - 不同重要程度的业务需要针对性处理日志.
@@ -37,12 +44,12 @@ abstract class BusinessException extends RuntimeException
      *
      * @var int
      */
-    protected int $importance = 0;
+    protected int $importance = self::DEFAULT_LEVEL;
 
     /**
      * 构造函数.
      */
-    public function __construct(?string $message = null, int $code = 0, int $importance = 0)
+    public function __construct(?string $message = null, int $code = 0, int $importance = self::DEFAULT_LEVEL)
     {
         parent::__construct($message, $code);
         $this->importance = $importance;
