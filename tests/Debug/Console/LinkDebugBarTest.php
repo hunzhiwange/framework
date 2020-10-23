21e4ead29daacf9dae34155dbe80b9173fd12b95
XiangMin Liu <635750556@qq.com>
2020-10-23 20:55:38 +0800

    tests(debug): add tests Tests\Debug\Console\LinkDebugBarTest

---
+++ tests/Debug/Console/LinkDebugBarTest.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ************************ package.
+ * _____________                           _______________
+ *  ______/     \__  _____  ____  ______  / /_  _________
+ *   ____/ __   / / / / _ \/ __`\/ / __ \/ __ \/ __ \___
+ *    __/ / /  / /_/ /  __/ /  \  / /_/ / / / / /_/ /__
+ *      \_\ \_/\____/\___/_/   / / .___/_/ /_/ .___/
+ *         \_\                /_/_/         /_/
+ *
+ * The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+ * (c) 2010-2020 http://queryphp.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Debug\Console;
+
+use Leevel\Debug\Console\LinkDebugBar;
+use Leevel\Di\IContainer;
+use Leevel\Filesystem\Helper;
+use Leevel\Kernel\App as Apps;
+use Leevel\Kernel\IApp;
+use Tests\Console\BaseCommand;
+use Tests\TestCase;
+
+class LinkDebugBarTest extends TestCase
+{
+    use BaseCommand;
+
+    protected function setUp(): void
+    {
+        $this->tearDown();
+    }
+
+    protected function tearDown(): void
+    {
+        $dirs = [
+            __DIR__.'/assert_new',
+        ];
+        foreach ($dirs as $dir) {
+            Helper::deleteDirectory($dir);
+        }
+    }
+
+    public function testBaseUse(): void
+    {
+        $result = $this->runCommand(
+            new LinkDebugBar(),
+            [
+                'command' => 'link:debugbar',
+            ],
+            function ($container) {
+                $this->initContainerService($container);
+            }
+        );
+
+        $result = $this->normalizeContent($result);
+
+        $this->assertStringContainsString(
+            $this->normalizeContent(sprintf('Linked `%s/assert/debugbar` directory to `%s/assert_new/debugbar1` successed.', __DIR__, __DIR__)),
+            $result
+        );
+        $this->assertStringContainsString(
+            $this->normalizeContent(sprintf('Linked `%s/assert/debugbar` directory to `%s/assert_new/debugbar2` successed.', __DIR__, __DIR__)),
+            $result
+        );
+        $this->assertTrue(is_file(__DIR__.'/assert_new/debugbar1'));
+        $this->assertTrue(is_file(__DIR__.'/assert_new/debugbar2'));
+    }
+
+    public function testTargetFileAlreadyExists(): void
+    {
+        $targetFile = __DIR__.'/assert_new/debugbar1';
+        Helper::createFile($targetFile, 'debugbar1');
+        $this->assertTrue(is_file($targetFile));
+
+        $result = $this->runCommand(
+            new LinkDebugBar(),
+            [
+                'command' => 'link:debugbar',
+            ],
+            function ($container) {
+                $this->initContainerService($container);
+            }
+        );
+
+        $result = $this->normalizeContent($result);
+
+        $this->assertStringContainsString(
+            $this->normalizeContent(sprintf('The `%s` directory already exists.', $targetFile)),
+            $result
+        );
+        $this->assertTrue(is_file($targetFile));
+    }
+
+    protected function initContainerService(IContainer $container): void
+    {
+        // 注册 app
+        $app = new AppForLinkDebugBar($container, '');
+        $this->assertInstanceof(IApp::class, $app);
+        $container->singleton(IApp::class, $app);
+    }
+}
+
+class AppForLinkDebugBar extends Apps
+{
+    public function path(string $path = ''): string
+    {
+        if ('www/debugbar' === $path) {
+            return __DIR__.'/assert_new/debugbar1';
+        }
+
+        if ('debugbar' === $path) {
+            return __DIR__.'/assert_new/debugbar2';
+        }
+
+        return __DIR__.'/assert/debugbar';
+    }
+
+    protected function registerBaseProvider(): void
+    {
+    }
+}
