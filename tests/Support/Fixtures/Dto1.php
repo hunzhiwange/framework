99d65fa25e883d78b669a83e7cdd880f4dced723
XiangMin Liu <635750556@qq.com>
2021-01-10 21:38:14 +0800

    tests(support): Add Tests\Support\DtoTest

---
+++ tests/Support/Fixtures/Dto1.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Support\Fixtures;
+
+use Leevel\Support\Dto;
+
+class Dto1 extends Dto
+{
+    public static array $testStatic = [];
+
+    public string $demoStringProp;
+
+    public int $demoIntProp;
+
+    public float $demoFloatProp;
+
+    public bool $demoTrueProp = true;
+
+    public bool $demoFalseProp = false;
+
+    public DtoProp1 $demoObjectProp;
+
+    public DtoProp2 $demoObject2Prop;
+
+    public Dto2 $demoObject3Prop;
+
+    public mixed $demoMixedProp = true;
+
+    protected string $demoProtectedProp;
+
+    private mixed $demoPrivateProp;
+}
