85f68873999b405911c26d5a53229a9be158585c
XiangMin Liu <635750556@qq.com>
2021-07-31 11:59:01 +0800

    feat: Doc workflow

--- tests/Database/SummaryDoc.php
+++ tests/Database/SummaryDoc.php
@@ -10,7 +10,7 @@ namespace Tests\Database;
  *     zh-CN:title="概述",
  *     zh-TW:title="概述",
  *     path="database/README",
- *     description="QueryPHP database is 666",
+ *     description="QueryPHP database is 666.",
  *     zh-CN:description="QueryPHP 数据库非常强大",
  *     zh-TW:description="QueryPHP 資料庫非常强大",
  * )
