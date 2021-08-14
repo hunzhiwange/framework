2c2159cb0e4533a28657c9d8f56e40a8553f1ea1
XiangMin Liu <635750556@qq.com>
2021-08-14 12:34:38 +0800

    docs(router): Fix router doc

--- tests/Router/RouterTest.php
+++ tests/Router/RouterTest.php
@@ -677,7 +677,7 @@ class RouterTest extends TestCase
 
     /**
      * @api(
-     *     zh-CN:title="控制器支持冒号分隔为子目录",
+     *     zh-CN:title="控制器支持波浪号分隔为子目录",
      *     zh-CN:description="
      * 子目录支持无限层级。
      *
@@ -724,7 +724,7 @@ class RouterTest extends TestCase
 
     /**
      * @api(
-     *     zh-CN:title="控制器支持冒号分隔为子目录多层级例子",
+     *     zh-CN:title="控制器支持波浪号分隔为子目录多层级例子",
      *     zh-CN:description="
      * 子目录支持无限层级。
      *
@@ -818,9 +818,9 @@ class RouterTest extends TestCase
 
     /**
      * @api(
-     *     zh-CN:title="方法支持冒号分隔转为驼峰规则",
+     *     zh-CN:title="方法支持波浪号分隔转为驼峰规则",
      *     zh-CN:description="
-     * 冒号分隔方法，方法未独立成类，则将冒号转为驼峰规则。
+     * 波浪号分隔方法，方法未独立成类，则将波浪号转为驼峰规则。
      *
      * 下面例子中的方法为 `fooBar`。
      *
@@ -852,9 +852,9 @@ class RouterTest extends TestCase
 
     /**
      * @api(
-     *     zh-CN:title="方法独立为类支持冒号分隔转为子目录",
+     *     zh-CN:title="方法独立为类支持波浪号分隔转为子目录",
      *     zh-CN:description="
-     * 冒号分隔方法，方法独立成类，则将冒号转为子目录。
+     * 波浪号分隔方法，方法独立成类，则将波浪号转为子目录。
      *
      * 子目录支持无限层级。
      *
@@ -963,7 +963,7 @@ class RouterTest extends TestCase
 
     /**
      * @api(
-     *     zh-CN:title="RESTFUL 控制器支持冒号分隔为子目录",
+     *     zh-CN:title="RESTFUL 控制器支持波浪号分隔为子目录",
      *     zh-CN:description="
      * 子目录支持无限层级。
      *
@@ -1010,9 +1010,9 @@ class RouterTest extends TestCase
 
     /**
      * @api(
-     *     zh-CN:title="RESTFUL 方法支持冒号分隔转为驼峰规则",
+     *     zh-CN:title="RESTFUL 方法支持波浪号分隔转为驼峰规则",
      *     zh-CN:description="
-     * 冒号分隔方法，方法未独立成类，则将冒号转为驼峰规则。
+     * 波浪号分隔方法，方法未独立成类，则将波浪号转为驼峰规则。
      *
      * 下面例子中的方法为 `fooBar`。
      *
@@ -1044,7 +1044,7 @@ class RouterTest extends TestCase
 
     /**
      * @api(
-     *     zh-CN:title="RESTFUL 方法支持冒号分隔为子目录",
+     *     zh-CN:title="RESTFUL 方法支持波浪号分隔为子目录",
      *     zh-CN:description="
      * 子目录支持无限层级。
      *
@@ -1076,7 +1076,7 @@ class RouterTest extends TestCase
 
     /**
      * @api(
-     *     zh-CN:title="应用支持冒号分隔为子目录",
+     *     zh-CN:title="应用支持波浪号分隔为子目录",
      *     zh-CN:description="
      * 子目录支持无限层级。
      *
