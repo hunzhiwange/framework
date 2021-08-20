640f18f47113d24bf287f2013efbe0fa126d418e
XiangMin Liu <635750556@qq.com>
2021-08-20 22:11:15 +0800

    refactor(database): Better ide helper

--- src/Leevel/Database/Ddd/Repository.php
+++ src/Leevel/Database/Ddd/Repository.php
@@ -56,7 +56,7 @@ use Leevel\Database\Page;
  * @method static mixed select(null|callable|\Leevel\Database\Select|string $data = null, array $bind = [], bool $flag = false)                                                                                         原生 SQL 查询数据.
  * @method static null|array|int insert(array|string $data, array $bind = [], bool $replace = false, bool $flag = false)                                                                         插入数据 insert (支持原生 SQL).
  * @method static null|array|int insertAll(array $data, array $bind = [], bool $replace = false, bool $flag = false)                                                                批量插入数据 insertAll.
- * @method static array|int update($data, array $bind = [], bool $flag = false)                                                                                                更新数据 update (支持原生 SQL).
+ * @method static array|int update(array|string $data, array $bind = [], bool $flag = false)                                                                                                更新数据 update (支持原生 SQL).
  * @method static array|int updateColumn(string $column, $value, array $bind = [], bool $flag = false)                                                                         更新某个字段的值
  * @method static array|int updateIncrease(string $column, int $step = 1, array $bind = [], bool $flag = false)                                                                字段递增.
  * @method static array|int updateDecrease(string $column, int $step = 1, array $bind = [], bool $flag = false)                                                                字段减少.
