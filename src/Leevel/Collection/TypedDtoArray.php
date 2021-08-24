a700e85afa8e0cd6b63c2702ffa2793e1f5be153
XiangMin Liu <635750556@qq.com>
2021-08-24 19:10:02 +0800

    feat(collection): Add Leevel\Collection\TypedDtoArray

---
+++ src/Leevel/Collection/TypedDtoArray.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Leevel\Collection;
+
+use Leevel\Support\Dto;
+
+/**
+ * 数据传输对象索引数组.
+ */
+class TypedDtoArray extends Collection
+{
+    /**
+     * 键类型.
+     */
+    protected array $keyTypes = ['int'];
+
+    /**
+     * 值类型.
+     */
+    protected array $valueTypes = [Dto::class];
+
+    /**
+     * 构造函数.
+     */
+    public function __construct(array $data)
+    {
+        parent::__construct($data);
+    }
+
+    /**
+     * 从 HTTP 请求创建数据传输对象数组.
+     */
+    public static function fromRequest(array $data, string $dtoClass): static
+    {
+        $dtoItems = [];
+        foreach ($data as $item) {
+            $dtoItems[] = new $dtoClass($item);
+        }
+
+        return new static($dtoItems);
+    }
+}
