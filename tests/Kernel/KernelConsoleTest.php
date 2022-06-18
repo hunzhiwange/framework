5b9155773a52808059ad66c5a7388147a01838d2
XiangMin Liu <635750556@qq.com>
2022-06-18 10:25:25 +0800

    tests: Fix test

--- tests/Kernel/KernelConsoleTest.php
+++ tests/Kernel/KernelConsoleTest.php
@@ -181,7 +181,7 @@ class AppKernelConsole extends Apps
 
 class Application1 extends Application
 {
-    public function run(?InputInterface $input = null, ?OutputInterface $output = null)
+    public function run(?InputInterface $input = null, ?OutputInterface $output = null): int
     {
         return 0;
     }
