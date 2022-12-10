529168828070d8115ba5599aaf5da943af981178
XiangMin Liu <635750556@qq.com>
2022-12-10 16:29:36 +0800

    更新roadrunner 支持

--- src/Leevel/Level/Console/RoadRunnerServer.php
+++ src/Leevel/Level/Console/RoadRunnerServer.php
@@ -5,20 +5,15 @@ declare(strict_types=1);
 namespace Leevel\Level\Console;
 
 use Exception;
-use Laminas\Diactoros\ResponseFactory;
-use Laminas\Diactoros\ServerRequestFactory;
-use Laminas\Diactoros\StreamFactory;
-use Laminas\Diactoros\UploadedFileFactory;
 use Leevel\Console\Command;
 use Leevel\Http\Request;
 use Leevel\Kernel\IApp;
 use Leevel\Kernel\IKernel;
-use Spiral\Goridge\StreamRelay;
-use Spiral\RoadRunner\PSR7Client;
-use Spiral\RoadRunner\Worker;
 use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
 use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
 use Throwable;
+use Spiral\RoadRunner;
+use Nyholm\Psr7\Factory\Psr17Factory;
 
 /**
  * RoadRunner.
@@ -46,24 +41,27 @@ class RoadRunnerServer extends Command
         $this->checkEnvironment();
         $this->setDisplayErrors();
         $kernel = $app->container()->make(IKernel::class);
-        $psr7 = $this->getPsr7();
         $httpFoundationFactory = new HttpFoundationFactory();
+        $psr17Factory = new Psr17Factory();
         $psrHttpFactory = new PsrHttpFactory(
-            new ServerRequestFactory(),
-            new StreamFactory(),
-            new UploadedFileFactory(),
-            new ResponseFactory(),
+            $psr17Factory,
+            $psr17Factory,
+            $psr17Factory,
+            $psr17Factory,
         );
+        $worker = RoadRunner\Worker::create();
+        $worker = new RoadRunner\Http\PSR7Worker($worker, $psr17Factory, $psr17Factory, $psr17Factory);
 
-        while ($req = $psr7->acceptRequest()) {
+        while ($req = $worker->waitRequest()) {
             try {
                 $symfonyRequest = $httpFoundationFactory->createRequest($req);
                 $request = Request::createFromSymfonyRequest($symfonyRequest);
                 $response = $kernel->handle($request);
-                $psr7->respond($psrHttpFactory->createResponse($response));
+                $rsp = $psrHttpFactory->createResponse($response);
+                $worker->respond($rsp);
                 $kernel->terminate($request, $response);
             } catch (Throwable $e) {
-                $psr7->getWorker()->error((string) $e);
+                $worker->getWorker()->error((string) $e);
             }
         }
 
@@ -77,12 +75,13 @@ class RoadRunnerServer extends Command
      */
     protected function checkEnvironment(): void
     {
-        if (!class_exists(Worker::class) ||
+        if (!class_exists(RoadRunner\Http\PSR7Worker::class) ||
             !class_exists(HttpFoundationFactory::class) ||
-            !class_exists(ResponseFactory::class)) {
+            !class_exists(Psr17Factory::class)) {
             $message = 'Go RoadRunner needs the following packages'.PHP_EOL.
                 'composer require spiral/roadrunner ^2.14.1'.PHP_EOL.
                 'composer require spiral/dumper ^2.12.1.'.PHP_EOL.
+                'composer require nyholm/psr7 ^1.5.'.PHP_EOL.
                 'composer require symfony/psr-http-message-bridge ^2.0';
 
             throw new Exception($message);
@@ -95,15 +94,5 @@ class RoadRunnerServer extends Command
     protected function setDisplayErrors(): void
     {
         ini_set('display_errors', 'stderr');
-    }
-
-    /**
-     * 取得 Psr 桥接.
-     */
-    protected function getPsr7(): PSR7Client
-    {
-        $relay = new StreamRelay(STDIN, STDOUT);
-
-        return new PSR7Client(new Worker($relay));
     }
 }
