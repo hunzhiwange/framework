3f6af01cfa2af21e81f38e3f8912b66be4902c1f
XiangMin Liu <635750556@qq.com>
2023-02-09 12:24:52 +0800

    新增消息属性

---
+++ src/Leevel/Support/Msg.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Leevel\Support;
+
+use Leevel\I18n\Gettext;
+
+/**
+ * 消息属性.
+ */
+#[\Attribute]
+class Msg
+{
+    private readonly string $message;
+
+    public function __construct(string $message, ...$moreArgs)
+    {
+        $this->message = __($message, ...$moreArgs);
+    }
+
+    public function __invoke(): string
+    {
+        return $this->message;
+    }
+}
+
+if (!\function_exists(__NAMESPACE__.'\\__')) {
+    function __(string $text, ...$data): string
+    {
+        if (!class_exists(Gettext::class)) {
+            return sprintf($text, ...$data);
+        }
+
+        return Gettext::handle($text, ...$data);
+    }
+}
