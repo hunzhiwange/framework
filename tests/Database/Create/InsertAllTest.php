af86a4f14b8e6964b1bd6e142fb6aed59b22c5ab
XiangMin Liu <635750556@qq.com>
2023-02-15 13:56:09 +0800

    添加测试用例 insertAll 支持 ON DUPLICATE KEY UPDATE 用

--- tests/Database/Create/InsertAllTest.php
+++ tests/Database/Create/InsertAllTest.php
@@ -435,6 +435,138 @@ final class InsertAllTest extends TestCase
         );
     }
 
+    /**
+     * @api(
+     *     zh-CN:title="insertAll 支持 ON DUPLICATE KEY UPDATE 用法",
+     *     zh-CN:description="",
+     *     zh-CN:note="",
+     * )
+     */
+    public function testInsertAllSupportDuplicateKeyUpdate(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
+
+        $sql = <<<'eot'
+            [
+                "INSERT INTO `test_query` (`test_query`.`name`,`test_query`.`value`) VALUES (:pdonamedparameter_name,:pdonamedparameter_value),(:pdonamedparameter_name_1,:pdopositional2namedparameter_0_1),(:pdonamedparameter_name_2,:pdonamedparameter_value_2),(:pdonamedparameter_name_3,:pdopositional2namedparameter_1_3) ON DUPLICATE KEY UPDATE `test_query`.`name` = VALUES(`test_query`.`name`),`test_query`.`value` = VALUES(`test_query`.`value`)",
+                {
+                    "pdonamedparameter_name": [
+                        "小鸭子1"
+                    ],
+                    "pdonamedparameter_value": [
+                        "呱呱呱1"
+                    ],
+                    "pdonamedparameter_name_1": [
+                        "小鸭子2"
+                    ],
+                    "pdopositional2namedparameter_0_1": [
+                        "吃鱼"
+                    ],
+                    "pdonamedparameter_name_2": [
+                        "小鸭子3"
+                    ],
+                    "pdonamedparameter_value_2": [
+                        "呱呱呱3"
+                    ],
+                    "pdonamedparameter_name_3": [
+                        "小鸭子4"
+                    ],
+                    "pdopositional2namedparameter_1_3": [
+                        "吃肉"
+                    ]
+                },
+                false
+            ]
+            eot;
+
+        $data = [
+            ['name' => '小鸭子1', 'value' => '呱呱呱1'],
+            ['name' => '小鸭子2', 'value' => Condition::raw('?')],
+            ['name' => '小鸭子3', 'value' => '呱呱呱3'],
+            ['name' => '小鸭子4', 'value' => Condition::raw('?')],
+        ];
+
+        static::assertSame(
+            $sql,
+            $this->varJsonSql(
+                $connect
+                    ->table('test_query')
+                    ->bind(['吃鱼', '吃肉'])
+                    ->insertAll($data, [], ['name', 'value']),
+                $connect
+            )
+        );
+    }
+
+    /**
+     * @api(
+     *     zh-CN:title="insertAll 支持 ON DUPLICATE KEY UPDATE 表达式用法",
+     *     zh-CN:description="",
+     *     zh-CN:note="",
+     * )
+     */
+    public function testInsertAllSupportDuplicateKeyUpdate2(): void
+    {
+        $connect = $this->createDatabaseConnectMock();
+
+        $sql = <<<'eot'
+            [
+                "INSERT INTO `test_query` (`test_query`.`name`,`test_query`.`value`) VALUES (:pdonamedparameter_name,:pdonamedparameter_value),(:pdonamedparameter_name_1,:pdopositional2namedparameter_0_1),(:pdonamedparameter_name_2,:pdonamedparameter_value_2),(:pdonamedparameter_name_3,:pdopositional2namedparameter_1_3) ON DUPLICATE KEY UPDATE `test_query`.`name` = (CONCAT(VALUES(`test_query`.`name`), 'lianjie', VALUES(`test_query`.`value`))),`test_query`.`value` = :value",
+                {
+                    "value": [
+                        5
+                    ],
+                    "pdonamedparameter_name": [
+                        "小鸭子1"
+                    ],
+                    "pdonamedparameter_value": [
+                        "呱呱呱1"
+                    ],
+                    "pdonamedparameter_name_1": [
+                        "小鸭子2"
+                    ],
+                    "pdopositional2namedparameter_0_1": [
+                        "吃鱼"
+                    ],
+                    "pdonamedparameter_name_2": [
+                        "小鸭子3"
+                    ],
+                    "pdonamedparameter_value_2": [
+                        "呱呱呱3"
+                    ],
+                    "pdonamedparameter_name_3": [
+                        "小鸭子4"
+                    ],
+                    "pdopositional2namedparameter_1_3": [
+                        "吃肉"
+                    ]
+                },
+                false
+            ]
+            eot;
+
+        $data = [
+            ['name' => '小鸭子1', 'value' => '呱呱呱1'],
+            ['name' => '小鸭子2', 'value' => Condition::raw('?')],
+            ['name' => '小鸭子3', 'value' => '呱呱呱3'],
+            ['name' => '小鸭子4', 'value' => Condition::raw('?')],
+        ];
+
+        static::assertSame(
+            $sql,
+            $this->varJsonSql(
+                $connect
+                    ->table('test_query')
+                    ->bind(['吃鱼', '吃肉'])
+                    ->insertAll($data, [], [
+                        'name' => Condition::raw("CONCAT(VALUES([name]), 'lianjie', VALUES([value]))"),
+                        'value' => 5,
+                    ]),
+                $connect
+            )
+        );
+    }
+
     protected function getDatabaseTable(): array
     {
         return ['test_query'];
