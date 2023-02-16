2fa13a47dea9cb42b87e67a8097974156e3f576c
XiangMin Liu <635750556@qq.com>
2023-02-16 23:40:37 +0800

    新增实体集合

---
+++ src/Leevel/Database/Ddd/EntityCollection.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Leevel\Database\Ddd;
+
+use Leevel\Support\Collection;
+
+/**
+ * 实体集合.
+ */
+class EntityCollection extends Collection
+{
+    /**
+     * 键类型.
+     */
+    protected array $keyTypes = ['int'];
+
+    /**
+     * 值类型.
+     */
+    protected array $valueTypes = [Entity::class];
+
+    /**
+     * 构造函数.
+     */
+    public function __construct(array $data)
+    {
+        parent::__construct($data);
+    }
+
+    /**
+     * 获取实体.
+     *
+     * @throws \Exception
+     */
+    public function get(int $key): Entity
+    {
+        $entity = $this->__get($key);
+        if (!$entity) {
+            throw new \Exception(sprintf('Entity %d was not found.', $key));
+        }
+
+        // @phpstan-ignore-next-line
+        return $entity;
+    }
+
+    /**
+     * 设置实体.
+     */
+    public function set(int $key, Entity $entity): void
+    {
+        $this->__set($key, $entity);
+    }
+
+    /**
+     * 删除实体.
+     */
+    public function remove(int $key): void
+    {
+        $this->__unset($key);
+    }
+
+    /**
+     * 是否存在实体.
+     */
+    public function has(int $key): bool
+    {
+        return $this->__isset($key);
+    }
+}
