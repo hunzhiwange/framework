fe09ee2fdbeca6267dd2aa95bde8d29439908beb
XiangMin Liu <635750556@qq.com>
2023-02-16 22:13:14 +0800

    更新一下

--- tests/Database/Ddd/Entity/Relation/User.php
+++ tests/Database/Ddd/Entity/Relation/User.php
@@ -6,6 +6,7 @@ namespace Tests\Database\Ddd\Entity\Relation;
 
 use Leevel\Database\Ddd\Entity;
 use Leevel\Database\Ddd\Relation\ManyMany;
+use Leevel\Database\Ddd\Struct;
 
 class User extends Entity
 {
@@ -126,6 +127,149 @@ class User extends Entity
         ],
     ];
 
+    #[Struct([
+    ])]
+    protected ?int $id = null;
+
+    #[Struct([
+    ])]
+    protected ?string $name = null;
+
+    #[Struct([
+    ])]
+    protected ?int $createAt = null;
+
+    #[Struct([
+        self::MANY_MANY => Role::class,
+        self::MIDDLE_ENTITY => UserRole::class,
+        self::SOURCE_KEY => 'id',
+        self::TARGET_KEY => 'id',
+        self::MIDDLE_SOURCE_KEY => 'user_id',
+        self::MIDDLE_TARGET_KEY => 'role_id',
+    ])]
+    protected ?string $role = null;
+
+    #[Struct([
+        self::MANY_MANY => RoleSoftDeleted::class,
+        self::MIDDLE_ENTITY => UserRoleSoftDeleted::class,
+        self::SOURCE_KEY => 'id',
+        self::TARGET_KEY => 'id',
+        self::MIDDLE_SOURCE_KEY => 'user_id',
+        self::MIDDLE_TARGET_KEY => 'role_id',
+    ])]
+    protected ?int $roleSoftDeleted = null;
+
+    #[Struct([
+        self::MANY_MANY => RoleSoftDeleted::class,
+        self::MIDDLE_ENTITY => UserRoleSoftDeleted::class,
+        self::SOURCE_KEY => 'id',
+        self::TARGET_KEY => 'id',
+        self::MIDDLE_SOURCE_KEY => 'user_id',
+        self::MIDDLE_TARGET_KEY => 'role_id',
+        self::RELATION_SCOPE => 'withSoftDeleted',
+    ])]
+    protected ?int $roleMiddleWithSoftDeleted = null;
+
+    #[Struct([
+        self::MANY_MANY => RoleSoftDeleted::class,
+        self::MIDDLE_ENTITY => UserRoleSoftDeleted::class,
+        self::SOURCE_KEY => 'id',
+        self::TARGET_KEY => 'id',
+        self::MIDDLE_SOURCE_KEY => 'user_id',
+        self::MIDDLE_TARGET_KEY => 'role_id',
+        self::RELATION_SCOPE => 'onlySoftDeleted',
+    ])]
+    protected ?int $roleMiddleOnlySoftDeleted = null;
+
+    #[Struct([
+        self::MANY_MANY => RoleSoftDeleted::class,
+        self::MIDDLE_ENTITY => UserRoleSoftDeleted::class,
+        self::SOURCE_KEY => 'id',
+        self::TARGET_KEY => 'id',
+        self::MIDDLE_SOURCE_KEY => 'user_id',
+        self::MIDDLE_TARGET_KEY => 'role_id',
+        self::RELATION_SCOPE => 'notFound',
+    ])]
+    protected ?int $roleRelationScopeNotFound = null;
+
+    #[Struct([
+        self::MANY_MANY => RoleSoftDeleted::class,
+        self::MIDDLE_ENTITY => UserRoleSoftDeleted::class,
+        self::SOURCE_KEY => 'id',
+        self::TARGET_KEY => 'id',
+        self::MIDDLE_SOURCE_KEY => 'user_id',
+        self::MIDDLE_TARGET_KEY => 'role_id',
+        self::RELATION_SCOPE => 'foundButPrivate',
+    ])]
+    protected ?int $roleRelationScopeFoundButPrivate = null;
+
+    #[Struct([
+        self::MANY_MANY => Role::class,
+        self::SOURCE_KEY => 'id',
+        self::TARGET_KEY => 'id',
+        self::MIDDLE_SOURCE_KEY => 'user_id',
+        self::MIDDLE_TARGET_KEY => 'role_id',
+    ])]
+    protected ?int $roleNotDefinedMiddleEntity = null;
+
+    #[Struct([
+        self::MANY_MANY => Role::class,
+        self::MIDDLE_ENTITY => UserRole::class,
+        self::TARGET_KEY => 'id',
+        self::MIDDLE_SOURCE_KEY => 'user_id',
+        self::MIDDLE_TARGET_KEY => 'role_id',
+    ])]
+    protected ?int $roleNotDefinedSourceKey = null;
+
+    #[Struct([
+        self::MANY_MANY => Role::class,
+        self::MIDDLE_ENTITY => UserRole::class,
+        self::SOURCE_KEY => 'id',
+        self::MIDDLE_SOURCE_KEY => 'user_id',
+        self::MIDDLE_TARGET_KEY => 'role_id',
+    ])]
+    protected ?int $roleNotDefinedTargetKey = null;
+
+    #[Struct([
+        self::MANY_MANY => Role::class,
+        self::MIDDLE_ENTITY => UserRole::class,
+        self::SOURCE_KEY => 'id',
+        self::TARGET_KEY => 'id',
+        self::MIDDLE_TARGET_KEY => 'role_id',
+    ])]
+    protected ?int $roleNotDefinedMiddleSourceKey = null;
+
+    #[Struct([
+        self::MANY_MANY => Role::class,
+        self::MIDDLE_ENTITY => UserRole::class,
+        self::SOURCE_KEY => 'id',
+        self::TARGET_KEY => 'id',
+        self::MIDDLE_SOURCE_KEY => 'user_id',
+    ])]
+    protected ?int $roleNotDefinedMiddleTargetKey = null;
+
+    #[Struct([
+        self::MANY_MANY => Role::class,
+        self::MIDDLE_ENTITY => UserRole::class,
+        self::SOURCE_KEY => 'id',
+        self::TARGET_KEY => 'id',
+        self::MIDDLE_SOURCE_KEY => 'user_id',
+        self::MIDDLE_TARGET_KEY => 'role_id',
+        self::RELATION_SCOPE => 'middleField',
+    ])]
+    protected ?int $roleMiddleField = null;
+
+    #[Struct([
+        self::MANY_MANY => RoleSoftDeleted::class,
+        self::MIDDLE_ENTITY => UserRoleSoftDeleted::class,
+        self::SOURCE_KEY => 'id',
+        self::TARGET_KEY => 'id',
+        self::MIDDLE_SOURCE_KEY => 'user_id',
+        self::MIDDLE_TARGET_KEY => 'role_id',
+        self::RELATION_SCOPE => 'middleOnlySoftDeletedAndMiddleFieldAndOtherTableCondition',
+    ])]
+    protected ?int $roleMiddleOnlySoftDeletedAndMiddleFieldAndOtherTableCondition = null;
+
     protected function relationScopeWithSoftDeleted(ManyMany $relation): void
     {
         $relation->middleWithSoftDeleted();
@@ -151,7 +295,7 @@ class User extends Entity
         ;
     }
 
-    private function relationScopeFoundButPrivate(ManyMany $relation): void
+    protected function relationScopeFoundButPrivate(ManyMany $relation): void
     {
     }
 }
